b970fdaa8614dfbe4de23ff9cdafa0351a99de92
ashish-0280 <120677812+ashish-0280@users.noreply.github.com>
2024-08-31 20:05:31 +0530

    Time: 6 ms (51.93%), Space: 54.5 MB (78.38%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
@@ -14,72 +14,24 @@
  * }
  */
 class Solution {
+    int max = Integer.MIN_VALUE;
     public int longestZigZag(TreeNode root) {
         int cnt = 0;
-        int max = Integer.MIN_VALUE;
-        List <Integer> list = new ArrayList<>();
-        solve(root, cnt, list);
-        for(int i=0; i<list.size(); i++){
-            max = Math.max(max, list.get(i));
-        }
+        solve(root, 0, false);
+        solve(root, 0, true);
         return max;
     }
-    public void solve(TreeNode root, int cnt, List<Integer> list){
+    public void solve(TreeNode root, int cnt, boolean isLeft){
         if(root == null){
             return;
         }
-        TreeNode root1 = root;
-        while(root1.left != null || root1.right != null){
-            if(root1.left != null){
-                root1 = root1.left;
-                if(root1 != null){
-                    cnt++;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-            if(root1.right != null){
-                root1 = root1.right;
-                if(root1 != null){
-                    cnt++;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
+        max = Math.max(max, cnt);
+        if(isLeft){
+            solve(root.left, cnt+1, false);
+            solve(root.right, 1, true);
+        } else {
+            solve(root.right, cnt+1, true);
+            solve(root.left, 1, false);
         }
-        list.add(cnt);
-        cnt = 0;
-        root1 = root;
-
-        while(root1.left != null || root1.right != null){
-            if(root1.right != null){
-                root1 = root1.right;
-                if(root1 != null){
-                    cnt++;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-            if(root1.left != null){
-                root1 = root1.left;
-                if(root1 != null){
-                    cnt++;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-        list.add(cnt);
-        cnt = 0;
-        solve(root.left, cnt, list);
-        solve(root.right, cnt, list);
     }
 }
