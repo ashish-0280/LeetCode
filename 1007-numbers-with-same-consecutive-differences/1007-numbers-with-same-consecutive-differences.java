ffabf4287412b2814b9c6e09dc3b85daee3ddc18
ashish-0280 <120677812+ashish-0280@users.noreply.github.com>
2024-09-02 15:43:51 +0530

    Time: 3 ms (60.29%), Space: 42.5 MB (33.64%) - LeetHub

--- 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.java
+++ 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.java
@@ -1,22 +1,28 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
         List<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i=1; i<=9; i++){
-            dfs(list, i, n-1, k);
+            q.add(i);
+        }
+        int len = 1;
+        while(!q.isEmpty() && len < n){
+            len++;
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int curr = q.remove();
+                int lastdigit = curr % 10;
+                if(lastdigit + k <= 9) q.add(curr*10 + lastdigit + k);
+                if(k != 0 && lastdigit - k >= 0) q.add(curr*10 + lastdigit - k);
+            }
+        }
+        while(!q.isEmpty()){
+            list.add(q.remove());
         }
         int[] array = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             array[i] = list.get(i);
         }
         return array;
-    }
-    public void dfs(List<Integer> list, int num, int n, int k){
-        if(n==0){
-            list.add(num);
-            return;
-        }
-        int lastdigit = num%10;
-        if(lastdigit + k <= 9) dfs(list, num*10 + lastdigit + k, n-1, k);
-        if(k!=0 && lastdigit - k >= 0) dfs(list, num*10 + lastdigit - k, n-1, k);
     }
 }
