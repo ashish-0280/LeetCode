dbca728b10fc33c1a586678bc05059ca7e395460
ashish-0280 <120677812+ashish-0280@users.noreply.github.com>
2024-09-06 13:18:43 +0530

    Time: 9 ms (97.96%), Space: 45.6 MB (40.35%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()){
-            return false;
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        for(char ch: word1.toCharArray()){
+            freq1[ch-'a']++;
         }
-        Set <Character> set1 = new HashSet<>();
-        Set <Character> set2 = new HashSet<>();
-        HashMap <Character, Integer> map1 = new HashMap<>();
-        HashMap <Character, Integer> map2 = new HashMap<>();
-        for(int i=0; i<word1.length(); i++){
-            map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0)+1);
-            set1.add(word1.charAt(i));
+        for(char ch: word2.toCharArray()){
+            freq2[ch-'a']++;
         }
-        for(int i=0; i<word2.length(); i++){
-            map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0)+1);
-            set2.add(word2.charAt(i));
+        for(int i=0; i<26; i++){
+            if((freq1[i] == 0 && freq2[i] != 0) || (freq2[i] == 0 && freq1[i] != 0)){
+                return false;
+            }
         }
-        if(!set1.equals(set2)){
-            return false;
-        }
-        List <Map.Entry<Character, Integer>> list1 = new ArrayList<>(map1.entrySet());
-        List <Map.Entry<Character, Integer>> list2 = new ArrayList<>(map2.entrySet());
-        Collections.sort(list1, (a, b) -> a.getValue().compareTo(b.getValue()));
-        Collections.sort(list2, (a, b) -> a.getValue().compareTo(b.getValue()));
-        for(int i=0; i<list1.size(); i++){
-            if(!list1.get(i).getValue().equals(list2.get(i).getValue())){
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        for(int i=0; i<26; i++){
+            if(freq1[i] != freq2[i]){
                 return false;
             }
         }
