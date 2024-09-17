2e5de337f3eafaf35b07c72de97f89b7529638f1
Ashish <120677812+ashish-0280@users.noreply.github.com>
2024-09-17 08:15:48 +0530

    Time: 5 ms (20.24%), Space: 42 MB (69.28%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,33 +1,15 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
         HashMap <String, Integer> map = new HashMap<>();
-        String str = "";
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i) == ' '){
-                map.put(str, map.getOrDefault(str, 0)+1);
-                str = "";
-            } else {
-                str += s1.charAt(i);
-            }
+        for(String s: (s1+" "+s2).split(" ")){
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
-        map.put(str, map.getOrDefault(str, 0)+1);
-        str = "";
-        for(int i=0; i<s2.length(); i++){
-            if(s2.charAt(i) == ' '){
-                map.put(str, map.getOrDefault(str, 0)+1);
-                str = "";
-            } else {
-                str += s2.charAt(i);
-            }
-        }
-        map.put(str, map.getOrDefault(str, 0)+1);
         List<String> l = new ArrayList<>();
-        for(Map.Entry<String, Integer> mapElement : map.entrySet()){
-            if(mapElement.getValue() == 1){
-                l.add(mapElement.getKey());
+        for(String s: map.keySet()){
+            if(map.get(s) == 1){
+                l.add(s);
             }
         }
-        String[] ans = l.toArray(new String[0]);
-        return ans;
+        return l.toArray(new String[0]);
     }
 }
