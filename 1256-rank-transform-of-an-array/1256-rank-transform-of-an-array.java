02e2b20e214a8f96eea280611b5c56d2e69bd452
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2024-10-02 11:59:09 +0530

    Time: 27 ms (61.24%), Space: 61.9 MB (63.8%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,24 +1,14 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        if(arr.length == 0){
-            return arr;
-        }
-        int ans[] = new int[arr.length];
-        int nums[][] = new int[arr.length][2];
+        int nums[] = Arrays.copyOf(arr, arr.length);
+        Map <Integer, Integer> map = new HashMap<>();
+        Arrays.sort(arr);
         for(int i=0; i<arr.length; i++){
-            nums[i][0] = i;
-            nums[i][1] = arr[i];
+            map.putIfAbsent(arr[i], map.size() + 1);
         }
-        Arrays.sort(nums, Comparator.comparingDouble(o -> o[1]));
-        int rank = 1;
-        ans[nums[0][0]] = rank;
-        for(int i=1; i<arr.length; i++){
-           if(nums[i][1] == nums[i-1][1]){
-            ans[nums[i][0]] = ans[nums[i-1][0]];
-            continue;
-           }
-           ans[nums[i][0]] = ans[nums[i-1][0]]+1;
+        for(int i=0; i<arr.length; i++){
+            nums[i] = map.get(nums[i]);
         }
-        return ans;
+        return nums;
     }
 }
