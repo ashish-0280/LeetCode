f7a4e32a50f46c49fb3f1055b1993b0f7257b3e5
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2024-10-05 09:34:34 +0530

    Time: 141 ms (15.95%), Space: 45 MB (20.4%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -8,27 +8,15 @@ class Solution {
         return false;
     }
     public boolean check(String s1, String s2){
-        char c1[] = s1.toCharArray();
-        char c2[] = s2.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        for(int i=0; i<c1.length; i++){
-            if(c1[i] != c2[i]) return false;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(int i=0; i<s1.length(); i++){
+           freq1[s1.charAt(i)-'a']++;
+           freq2[s2.charAt(i)-'a']++;
         }
+        for(int i=0; i<26; i++){
+            if(freq1[i] != freq2[i]) return false;
+        } 
         return true;
-    }
-    public String permutations(String s, String s2){
-        if(s.length() == 1){
-            return s;
-        }
-        for(int i=0; i<s.length(); i++){
-            char currentChar = s.charAt(i);
-            String remainingString = s.substring(0, i) + s.substring(i+1);
-            String t = permutations(remainingString, s2);
-            if(s2.contains(t + currentChar)){
-                return "ok";
-            }
-        }
-        return "no";
     }
 }
