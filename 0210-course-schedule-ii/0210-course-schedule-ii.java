08ad747006549ddcfabb4ea93f4b91983bacab5d
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-05 21:13:52 +0530

    Time: 4 ms (91.82%), Space: 45.8 MB (70.65%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -8,34 +8,57 @@ class Solution {
         for(int i=0; i<prerequisites.length; i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        System.out.println(adj);
-        int indegree[] = new int[adj.size()];
+        
+        boolean vis1[] = new boolean[adj.size()];
+        boolean rec[] = new boolean[adj.size()];
         for(int i=0; i<adj.size(); i++){
-            for(int neighbour: adj.get(i)){
-                indegree[neighbour]++;
-            }
-        }
-        Queue <Integer> q = new LinkedList<>();
-        for(int i=0; i<adj.size(); i++){
-            if(indegree[i] == 0){
-                q.offer(i);
-            }
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        while(!q.isEmpty()){
-            int data = q.poll();
-            list.add(data);
-            for(int j=0; j<adj.get(data).size(); j++){
-                indegree[adj.get(data).get(j)]--;
-                if(indegree[adj.get(data).get(j)] == 0){
-                    q.add(adj.get(data).get(j));
+            if(!vis1[i]){
+                if(cycle(adj, vis1, i, rec)){
+                    return new int[0];
                 }
             }
         }
-        int arr[] = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            arr[i] = list.get(i);
+        boolean vis[] = new boolean[adj.size()];
+        Stack <Integer> s = new Stack<>();
+        for(int i=0; i<adj.size(); i++){
+            if(!vis[i]){
+                dfs(adj, i, vis, s);
+            }
         }
-        return list.size() == adj.size() ? arr : new int[0];
+        int arr[] = new int[s.size()];
+        int i=0;
+        while(!s.isEmpty()){
+            arr[i] = s.pop();
+            i++;
+        }
+        return arr;
+    }
+    public static void dfs(List<List<Integer>> adj, int curr, boolean vis[], Stack<Integer> s){
+        vis[curr] = true;
+        for(int i=0; i<adj.get(curr).size(); i++){
+            int neighbour = adj.get(curr).get(i);
+            if(!vis[neighbour]){
+                vis[neighbour] = true;
+                dfs(adj, neighbour, vis, s);
+            } 
+        }
+        if(vis[curr]){
+            s.push(curr);
+        }
+    }
+    public boolean cycle(List<List<Integer>> adj, boolean vis[], int curr, boolean rec[]){
+        vis[curr] = true; rec[curr] = true;
+        for(int i=0; i<adj.get(curr).size(); i++){
+            int neighbour = adj.get(curr).get(i);
+            if(!vis[neighbour]){
+                if(cycle(adj, vis, neighbour, rec)){
+                    return true;
+                }
+            } else if(rec[neighbour]){
+                return true;
+            }
+        }
+        rec[curr] = false;
+        return false;
     }
 }
