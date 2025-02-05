114f0839b92333798ef34661aee345c1475cc67a
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-05 17:25:22 +0530

    Time: 9 ms (5.03%), Space: 45.6 MB (94.1%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,20 +1,33 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int cnt = 0;
-        boolean vis[] = new boolean[isConnected.length];
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0; i<isConnected.length; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j=0; j<isConnected[i].length; j++){
+                if(i!=j){
+                    if(isConnected[i][j] == 1){
+                        list.add(j);
+                    }
+                }
+            }
+            adj.add(list);
+        }
+        System.out.println(adj);
+        int cnt = 0;
+        boolean vis[] = new boolean[adj.size()];
+        for(int i=0; i<adj.size(); i++){
             if(!vis[i]){
                 cnt++;
-                dfs(isConnected, i, vis);
+                dfs(adj, i, vis);
             }
         }
         return cnt;
     }
-    public void dfs(int[][] adj, int curr, boolean vis[]){
+    public void dfs(List<List<Integer>> adj, int curr, boolean vis[]){
         vis[curr] = true;
-        for(int i=0; i<adj[curr].length; i++){
-            int neighbour = i;
-            if(!vis[neighbour] && adj[curr][neighbour]==1){
+        for(int i=0; i<adj.get(curr).size(); i++){
+            int neighbour = adj.get(curr).get(i);
+            if(!vis[neighbour]){
                 dfs(adj, neighbour, vis);
             }
         }
