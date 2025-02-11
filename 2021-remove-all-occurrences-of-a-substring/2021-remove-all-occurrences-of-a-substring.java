5d6b6d2ee2fd1e1f727e7e85c910bef6319578fa
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-11 18:01:36 +0530

    Time: 1 ms (99.46%), Space: 42.2 MB (81.28%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,29 +1,10 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        Stack <Character> stk = new Stack<>();
-        int n = s.length()-1;
-        for(int i=0; i<=n; i++){
-            char ch = s.charAt(i);
-            stk.push(ch);
-            if(ch == part.charAt(part.length()-1)){
-                int j=part.length()-1;
-                List<Character> list = new ArrayList<>();
-                while(j >=0 && !stk.isEmpty() && stk.peek() == part.charAt(j)){
-                    list.add(stk.pop());
-                    j--;
-                }
-                if(j != -1){
-                    for(int k=list.size()-1; k>=0; k--){
-                        stk.push(list.get(k));
-                    }
-                }
-            }
-            System.out.println(stk);
+        StringBuilder sb = new StringBuilder(s);
+        while(sb.indexOf(part) != -1){
+            int idx = sb.indexOf(part);
+            sb.delete(idx, idx+part.length());
         }
-        StringBuilder sb = new StringBuilder();
-        while(!stk.isEmpty()){
-            sb.append(stk.pop());
-        }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
