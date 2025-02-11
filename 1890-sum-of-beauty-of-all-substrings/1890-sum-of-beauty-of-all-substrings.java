f55c659e6aa29dbff24943e4eaeac249a98ac4bb
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-11 20:52:37 +0530

    Time: 296 ms (26.43%), Space: 44.9 MB (25.47%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -2,16 +2,22 @@ class Solution {
     public int beautySum(String s) {
         int beauty = 0;
         for(int i=0; i<s.length(); i++){
-            int freq[] = new int[26];
+            Map <Character, Integer> map = new HashMap<>();
+            TreeMap<Integer, Integer> count = new TreeMap<>();
             for(int j=i; j<s.length(); j++){
-                freq[s.charAt(j)-'a']++;
-                int max = 0; int min = Integer.MAX_VALUE;
-                for(int count: freq){
-                    if(count>0){
-                        max = Math.max(max, count);
-                        min = Math.min(min, count);
+                if(map.containsKey(s.charAt(j))){
+                    int freq = map.get(s.charAt(j));
+                    count.put(freq, count.get(freq)-1);
+                    if(count.get(freq) == 0){
+                        count.remove(freq);
                     }
+                    map.put(s.charAt(j), freq+1);
+                } else {
+                    map.put(s.charAt(j), 1);
                 }
+                int newfreq = map.get(s.charAt(j));
+                count.put(newfreq, count.getOrDefault(newfreq, 0)+1);
+                int min = count.firstKey(); int max = count.lastKey();
                 beauty += max-min;
             }
         }
