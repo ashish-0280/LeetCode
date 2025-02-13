3f48aca2a4a7601806ca16607675c9f88f06d95f
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-13 06:55:00 +0530

    Time: 292 ms (5.35%), Space: 67.2 MB (97.12%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -4,12 +4,29 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             pq.add((long)nums[i]);
         }
-        while(pq.size() >= 2 && pq.peek()<(long)k){
+        while(pq.size() >= 2 && !check(pq, k)){
             long data1 = pq.poll();
             long data2 = pq.poll();
             long data = 2*Math.min(data1, data2) + Math.max(data1, data2);
             pq.offer(data);
         }
         return nums.length - pq.size();
+    }
+    public boolean check(PriorityQueue <Long> pq, int k){
+        List<Long> list = new ArrayList<>();
+        while(!pq.isEmpty()){
+            long data = pq.poll();
+            list.add(data);
+            if(data < (long)k){
+                for(long num: list){
+                    pq.add(num);
+                }
+                return false;
+            }
+        }
+        for(long num: list){
+            pq.add(num);
+        }
+        return true;
     }
 }
