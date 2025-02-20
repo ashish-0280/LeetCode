47e86bb11e55be1007156973479545b5d88f902e
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-02-20 10:01:24 +0530

    Time: 0 ms (100%), Space: 43 MB (82.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,36 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<>();
-        solve(list, n*2, 0, new StringBuilder());
+        solve(list, n, 0, 0, new StringBuilder());
         return list;
     }
-    public void solve(List<String> list, int n, int idx, StringBuilder s){
-        if(s.length() == n){
-            if(solve2(s.toString())){
-                list.add(s.toString());
-            }
+    public void solve(List<String> list, int n, int idx1, int idx2, StringBuilder s){
+        if(s.length() == n*2){
+            list.add(s.toString());
             return;
         }
-        if(idx >= n){
-            return;
+        if(idx1 < n){
+            solve(list, n, idx1+1, idx2, s.append('('));
+            s.deleteCharAt(s.length()-1);
         }
-        solve(list, n, idx+1, s.append('('));
-        s.deleteCharAt(s.length()-1);
-        solve(list, n, idx+1, s.append(')'));
-        s.deleteCharAt(s.length()-1);
-    }
-    public boolean solve2(String s){
-        Stack<Character> stk = new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            if(!stk.isEmpty() && stk.peek() == '(' && s.charAt(i) == ')'){
-                stk.pop();
-            }
-            else if(s.charAt(i) == '('){
-                stk.push(s.charAt(i));
-            } else {
-                return false;
-            }
+        if(idx2 < idx1){
+            solve(list, n, idx1, idx2+1, s.append(')'));
+            s.deleteCharAt(s.length()-1);
         }
-        return stk.isEmpty();
     }
 }
