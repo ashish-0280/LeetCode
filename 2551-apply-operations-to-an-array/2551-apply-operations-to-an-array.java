ac714c1a82a3ac112ad0ceb29428b61b26b168cd
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-03-02 01:49:13 +0530

    Time: 1 ms (96.52%), Space: 44.4 MB (8.8%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -8,12 +8,15 @@ class Solution {
             }
         }
         int j=0;
-        for(int num: nums){
-            if(num != 0){
-                arr[j] = num;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
                 j++;
             }
         }
-        return arr;
+        for(int i=j; i<nums.length; i++){
+            nums[i] = 0;
+        }
+        return nums;
     }
 }
