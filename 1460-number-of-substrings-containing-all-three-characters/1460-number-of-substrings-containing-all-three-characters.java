108295f05cebee0b5847d6d2bf19cd57c3f0e343
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-03-11 10:00:39 +0530

    Time: 1347 ms (5.61%), Space: 45.5 MB (6.87%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,17 +1,55 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int cnt = 0;
-        int i=0; 
-        int n = s.length();
+        if(s.length() < 3){
+            return 0;
+        }
         int arr[] = new int[3];
-        for(int j=0; j<s.length(); j++){
-            char ch = s.charAt(j);
-            arr[ch-'a']++;
-            while(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1){
-                cnt += n-j;
-                arr[s.charAt(i)-'a']--; i++;
+        int j = 0; int i = 0;
+        while(j<3){
+            if(s.charAt(j) == 'a'){
+                arr[0]++;
+            } else if(s.charAt(j) == 'b'){
+                arr[1]++;
+            } else {
+                arr[2]++;
             }
+            j++;
+        }
+        while(i < s.length()-2){
+            while(j<=s.length()){
+                if(check(arr)){
+                    cnt += s.length()-j+1;
+                    System.out.println(cnt);
+                    System.out.println(j);
+                    break;
+                }
+                if(j==s.length()) break;
+                char ch = s.charAt(j);
+                if(ch == 'a'){
+                    arr[0]++;
+                } else if(ch == 'b'){
+                    arr[1]++;
+                } else {
+                    arr[2]++;
+                }
+                j++;
+            }
+            if(s.charAt(i) == 'a'){
+                arr[0]--;
+            } else if(s.charAt(i) == 'b'){
+                arr[1]--;
+            } else {
+                arr[2]--;
+            }
+            i++;
         }
         return cnt;
+    }
+    public boolean check(int arr[]){
+        if(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1){
+            return true;
+        }
+        return false;
     }
 }
