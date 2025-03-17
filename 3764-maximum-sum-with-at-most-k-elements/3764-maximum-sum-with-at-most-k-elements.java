693c3682efb52e907e17662fc0b4917f16658d11
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-03-17 15:49:40 +0530

    Time: 476 ms (5.01%), Space: 81.5 MB (5.2%) - LeetHub

--- 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.java
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.java
@@ -1,27 +1,22 @@
 class Solution {
     public long maxSum(int[][] grid, int[] limits, int k) {
-        long max = 0;
-        List<PriorityQueue<Integer>> list = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
         for(int i=0; i<grid.length; i++){
-            list.add(new PriorityQueue<>(Collections.reverseOrder()));
             for(int j=0; j<grid[0].length; j++){
-                list.get(i).add(grid[i][j]);
+                pq.add(new int[]{grid[i][j], i});
             }
         }
-        while(k>0){
-            long currMax = 0; int p=-1;
-            for(int i=0; i<list.size(); i++){
-                if(limits[i] == 0) continue;
-                if(!list.get(i).isEmpty() && list.get(i).peek() > currMax){
-                    currMax = list.get(i).peek();
-                    p = i;
-                }
+        System.out.println(pq);
+        long sum = 0;
+        while(k>0 && !pq.isEmpty()){
+            int arr[] = pq.poll();
+            if(limits[arr[1]] != 0){
+                sum += arr[0];
+            } else {
+                continue;
             }
-            if(p == -1) break;
-            max += (long)list.get(p).poll();
-            limits[p]--;
-            k--;
+            limits[arr[1]]--; k--;
         }
-        return max;
+        return sum;
     }
 }
