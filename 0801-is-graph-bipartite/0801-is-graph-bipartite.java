c9c12ddbaacfc0d351aaf6caab2f71822b188aeb
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-03-24 09:56:25 +0530

    Time: 0 ms (100%), Space: 45.3 MB (28.81%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,31 +1,24 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        List<List<Integer>> g = new ArrayList<>();
-        for(int i=0; i<graph.length; i++){
-            g.add(new ArrayList<>());
-            for(int j=0; j<graph[i].length; j++){
-                g.get(i).add(graph[i][j]);
-            }
-        }
-        int color[] = new int[g.size()];
+        int color[] = new int[graph.length];
         Arrays.fill(color, -1);
-        for(int i=0; i<g.size(); i++){
+        for(int i=0; i<graph.length; i++){
             if(color[i] == -1){
-                if(!solve(g, color, i, 0)){
+                if(!dfs(graph, color, i, 0)){
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean solve(List<List<Integer>> graph, int color[], int curr, int currColor){
+    public boolean dfs(int[][] graph, int color[], int curr, int currColor){
         color[curr] = currColor;
-        for(int neighbour: graph.get(curr)){
-            if(color[curr] == color[neighbour]){
+        for(int v: graph[curr]){
+            if(color[v] == color[curr]){
                 return false;
             }
-            if(color[neighbour] == -1){
-                if(!solve(graph, color, neighbour, 1 - currColor)){
+            if(color[v] == -1){
+                if(!dfs(graph, color, v, 1-currColor)){
                     return false;
                 }
             }
