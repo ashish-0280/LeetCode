46b11c34ff851e696b60912e05caaefc5f82d0b1
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-03-27 08:36:22 +0530

    Time: 12 ms (80.13%), Space: 57.5 MB (59.62%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        int n = nums.size(); int x1 = 0; int x2 = 0;
-        int element = solve(nums);
+        int element = solve(nums); int cnt = 0;
+        int n = nums.size();
         for(int num: nums){
-            map2.put(num, map2.getOrDefault(num, 0)+1);
-        }
-        for(int i=0; i<nums.size()-1; i++){
-            map1.put(nums.get(i), map1.getOrDefault(nums.get(i), 0)+1);
-            map2.put(nums.get(i), map2.get(nums.get(i))-1);
-            if(map2.get(nums.get(i)) == 0){
-                map2.remove(nums.get(i));
+            if(element == num){
+                cnt++;
             }
-            if(map1.containsKey(element) && (map1.get(element) > (i+1)/2) && map2.containsKey(element) && (map2.get(element) > (n-(i+1))/2)){
+        }
+        int cnt1 = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(element == nums.get(i)){
+                cnt1++;
+            }
+            if(cnt1 > (i+1)/2 && cnt-cnt1 > (n-(i+1))/2){
                 return i;
             }
         }
