f57a6eb04671650b196106216d6e3eee46e18fe1
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-04-12 12:33:04 +0530

    Time: 76 ms (12.24%), Space: 68.6 MB (14.13%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
@@ -1,16 +1,36 @@
 class Solution {
+    int dp[][];
     public int minFlipsMonoIncr(String s) {
-        int flips = 0;
-        int ones = 0;
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '1') {
-                ones++; 
+        dp = new int[s.length()+1][2];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        return solve(s, 0, '0');
+    }
+    public int solve (String s, int idx, char prev){
+        if(idx == s.length()){
+            return 0;
+        }
+        if(dp[idx][prev-'0'] != -1){
+            return dp[idx][prev-'0'];
+        }
+        int flip = Integer.MAX_VALUE;
+        int notFlip = Integer.MAX_VALUE;
+        if(s.charAt(idx) == '0'){
+            if(prev == '0'){
+                flip = 1 + solve(s, idx+1, '1');
+                notFlip = solve(s, idx+1, '0');
             } else {
-                flips = Math.min(flips + 1, ones);
+                flip = 1 + solve(s, idx+1, '1');
+            }
+        } else {
+            if(prev == '0'){
+                flip = 1 + solve(s, idx+1, '0');
+                notFlip = solve(s, idx+1, '1');
+            } else {
+                notFlip = solve(s, idx+1, '1');
             }
         }
-
-        return flips;
+        return dp[idx][prev-'0'] = Math.min(flip, notFlip);
     }
 }
