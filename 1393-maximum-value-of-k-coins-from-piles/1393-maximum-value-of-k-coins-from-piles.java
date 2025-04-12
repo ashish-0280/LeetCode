624bb0b998dfe6b2975f70dcfd563819592a4d3b
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-04-12 14:41:56 +0530

    Time: 328 ms (16.51%), Space: 53.1 MB (72.33%) - LeetHub

--- 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.java
+++ 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.java
@@ -1,28 +1,20 @@
 class Solution {
-    int dp[][];
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        dp = new int[piles.size()+1][k+1];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        int n = piles.size();
+        int dp[][] = new int[n+1][k+1];
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
         }
-        return solve(piles, k, 0);
-    }
-    public int solve(List<List<Integer>> list, int k, int idx){
-        if(idx >= list.size()){
-            return 0;
+        for(int i=1; i<=n; i++){
+            for(int coins = 0; coins <= k; coins++){
+                int sum = 0;
+                dp[i][coins] = dp[i-1][coins];
+                for(int currCoins = 1; currCoins<=Math.min(coins, piles.get(i-1).size()); currCoins++){
+                    sum += piles.get(i-1).get(currCoins-1);
+                    dp[i][coins] = Math.max(dp[i][coins], sum + dp[i-1][coins-currCoins]);
+                }
+            }
         }
-        if(dp[idx][k] != -1){
-            return dp[idx][k];
-        }
-        int ans1 = 0;
-        int ans2 = 0;
-        ans1 = solve(list, k, idx+1); int sum = 0;
-        int max = 0; 
-        for(int i=0; i<Math.min(k, list.get(idx).size()); i++){
-            sum += list.get(idx).get(i);
-            ans2 = sum + solve(list, k-(i+1), idx+1);
-            max = Math.max(max, ans2);
-        }
-        return dp[idx][k] = Math.max(ans1, max);
+        return dp[n][k];
     }
 }
