4dc07e995f180354aebad7bbdd2792c2b628c734
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-04-14 17:04:07 +0530

    Time: 45 ms (5.54%), Space: 40.8 MB (19.07%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,35 +1,30 @@
 class Solution {
     Integer dp[][][];
     public int countNumbersWithUniqueDigits(int n) {
-        String s = "";
-        for(int i=0; i<n; i++){
-            s += '9';
-        }
         dp = new Integer[n][2][1<<10];
-        return solve(s, 0, 1, 0);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            sb.append("9");
+        }
+        String s = sb.toString();
+        int mask = 0; int tight = 1; int idx = 0;
+        return solve(s, idx, tight, mask);
     }
     public int solve(String s, int idx, int tight, int mask){
-        if (idx == s.length()) {
-            return 1; // valid number constructed
-        }
-
-        if (dp[idx][tight][mask] != null) return dp[idx][tight][mask];
-
-        int limit = (tight == 1) ? s.charAt(idx) - '0' : 9;
+        if(idx == s.length()) return 1;
+        if(dp[idx][tight][mask] != null) return dp[idx][tight][mask];
         int ans = 0;
-
-        for (int d = 0; d <= limit; d++) {
-            int newTight = (tight == 1 && d == limit) ? 1 : 0;
-
-            // skip leading zeros: allow zero if nothing has started yet
-            if (mask == 0 && d == 0) {
-                ans += solve(s, idx + 1, newTight, 0);
+        int limit = (tight == 1) ? s.charAt(0)-'0' : 9;
+        for(int i=0; i<=limit; i++){
+        int newTight = (tight == 1 && i==limit) ? 1 : 0;
+            if(mask == 0 && limit == i){
+                ans += solve(s, idx+1, newTight, 0);
             } else {
-                if ((mask & (1 << d)) == 0) {
-                    ans += solve(s, idx + 1, newTight, mask | (1 << d));
+                if((mask & (1<<i)) == 0){
+                    ans += solve(s, idx+1, newTight, mask | (1<<i));
                 }
             }
         }
-        return dp[idx][tight][mask] = ans;
+        return ans;
     }
 }
