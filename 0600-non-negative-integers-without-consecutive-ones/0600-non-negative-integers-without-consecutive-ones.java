0c0c90b7becff56ee4881253cadf0d2ffd65fc55
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-04-17 08:04:23 +0530

    Time: 3 ms (41.06%), Space: 41.9 MB (15.23%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -1,9 +1,9 @@
 class Solution {
-    Integer dp[][];
+    Integer dp[][][];
     public int findIntegers(int n) {
         String s = Integer.toBinaryString(n);
         int len = s.length();
-        dp = new Integer[len+1][2];
+        dp = new Integer[len+1][2][2];
         return solve(s, 0, 1, 0);
     }
     public int solve(String s, int idx, int tight, int prevBit){
@@ -11,8 +11,8 @@ class Solution {
             return 1;
         }
         int ans = 0;
-        if(dp[idx][prevBit] != null && tight != 1){
-            return dp[idx][prevBit];
+        if(dp[idx][prevBit][tight] != null){
+            return dp[idx][prevBit][tight];
         }
         int limit = (tight == 1) ? s.charAt(idx)-'0' : 1;
         for(int i=0; i<=limit; i++){
@@ -22,7 +22,6 @@ class Solution {
             int newTight = (i == limit && tight == 1) ? 1 : 0;
             ans += solve(s, idx+1, newTight, i);
         }
-        if(tight == 0) dp[idx][prevBit] = ans;
-        return ans;
+        return dp[idx][prevBit][tight] = ans;
     }
 }
