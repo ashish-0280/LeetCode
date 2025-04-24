cb2cbd9e1597a71c33cc908dcb38215c2242a322
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-04-24 09:07:03 +0530

    Time: 60 ms (5.85%), Space: 45.3 MB (98.47%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -1,23 +1,23 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return solveAtLeastKDistinct(nums, k) - solveAtLeastKDistinct(nums, k+1);
+        return solveAtMostKDistinct(nums, k) - solveAtMostKDistinct(nums, k-1);
     }
-    public int solveAtLeastKDistinct(int nums[], int k){
+    public int solveAtMostKDistinct(int nums[], int k){
         HashMap<Integer, Integer> map = new HashMap<>();
         int i=0; int j=0; int cnt = 0; int n = nums.length;
         while(j<n){
+            if(map.getOrDefault(nums[j], 0) == 0){
+                k--;
+            }
             map.put(nums[j], map.getOrDefault(nums[j], 0)+1);
-            while(map.size() == k){
-                cnt += n-j;
-                int num = nums[i];
-                if(map.containsKey(num)){
-                    map.put(num, map.get(num)-1);
-                }
-                if(map.get(num) == 0){
-                    map.remove(num);
+            while(k<0){
+                map.put(nums[i], map.get(nums[i])-1);
+                if(map.get(nums[i]) == 0){
+                    k++;
                 }
                 i++;
             }
+            cnt += j-i+1;
             j++;
         }
         return cnt;
