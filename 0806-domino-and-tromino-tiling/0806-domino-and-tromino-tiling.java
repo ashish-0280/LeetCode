cd103d4dd0387f94ab67fe42c23d4263e5368f73
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-05 08:06:13 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.4%) - LeetHub

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -0,0 +1,13 @@
+class Solution {
+    int mod = 1000000007;
+    public int numTilings(int n) {
+        int dp[] = new int[n+1];
+        if(n==1 || n==2) return n;
+        if(n==3) return 5;
+        dp[1] = 1; dp[2] = 2; dp[3] = 5;
+        for(int i=4; i<=n; i++){
+            dp[i] = ((2*dp[i-1])%mod + dp[i-3])%mod;
+        }
+        return dp[n];
+    }
+}
