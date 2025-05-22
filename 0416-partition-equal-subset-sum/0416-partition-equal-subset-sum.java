1b49ab8d91d78ce3ef45bcc9012a7a9072b76dff
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-22 14:06:24 +0530

    Time: 107 ms (10.98%), Space: 76 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,53 +1,23 @@
 class Solution {
+    Boolean dp[][];
     public boolean canPartition(int[] nums) {
-        int S = summation(nums);
-        if(S % 2 != 0){
-            return false;
-        }
-        int target = S/2;
-        int dp[][] = new int[nums.length+1][target+1];
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return isSubsetSum(nums.length, nums, target, dp);
-    }
-     public Boolean isSubsetSum(int N, int arr[], int sum, int dp[][]){
-        if(sum == 0){
-            return true;
-        }
-        if(N == 0){
-            return false;
-        }
-        
-        if(dp[N][sum] != -1){
-            return dp[N][sum] == 1;
-        } 
-        if(arr[N-1] > sum){
-            boolean p = isSubsetSum(N-1, arr, sum, dp);
-            if(p == true){
-                dp[N][sum] = 1;
-                return p;
-            } else {
-                dp[N][sum] = 0;
-                return p;
-            }
-        } else {
-            boolean take = isSubsetSum(N-1, arr, sum-arr[N-1], dp);
-            boolean skip = isSubsetSum(N-1, arr, sum, dp);
-
-            dp[N][sum] = (take || skip) ? 1 : 0;
-            
-        }
-        
-        return dp[N][sum] == 1;
-    }
-    public int summation(int arr[]){
         int sum = 0;
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
+        for(int num: nums){
+            sum += num;
         }
-        return sum;
+        if(sum % 2 != 0) return false;
+        dp = new Boolean[nums.length+1][sum+1];
+        int target = sum/2;
+        return solve(nums, target, 0);
+    }
+    public boolean solve(int nums[], int target, int idx){
+        if(target == 0) return true;
+        if(idx >= nums.length || target < 0){
+            return false;
+        }
+        if(dp[idx][target] != null) return dp[idx][target];
+        boolean include = solve(nums, target-nums[idx], idx+1);
+        boolean exclude = solve(nums, target, idx+1);
+        return dp[idx][target] = include || exclude;
     }
 }
