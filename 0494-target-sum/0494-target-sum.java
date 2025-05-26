2c45a8c0f39b30fee209a200350e05916e606aad
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-26 11:58:23 +0530

    Time: 11 ms (60.12%), Space: 54.7 MB (6.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,17 @@
 class Solution {
-    Integer[][] dp;
-    int offset = 1000;
-
+    Integer dp[][];
     public int findTargetSumWays(int[] nums, int target) {
-        dp = new Integer[nums.length][2001];
-        return solve(nums, 0, target);
+        dp = new Integer[nums.length][20001];
+        return solve(nums, 0, 0, target);
     }
-
-    public int solve(int[] nums, int idx, int target) {
-        if (idx == nums.length) {
-            return target == 0 ? 1 : 0;
+    public int solve(int nums[], int idx, int sum, int target){
+        if(idx == nums.length){
+            return sum==target ? 1 : 0;
         }
+        if(dp[idx][sum+1001] != null) return dp[idx][sum+1001];
+        int take = solve(nums, idx+1,  sum-nums[idx], target);
+        int notTake = solve(nums, idx+1, sum + nums[idx], target);
+        return dp[idx][sum+1001] = take + notTake;
 
-        if (target + offset < 0 || target + offset > 2000) {
-            return 0;
-        }
-
-        if (dp[idx][target + offset] != null) {
-            return dp[idx][target + offset];
-        }
-
-        int add = solve(nums, idx + 1, target - nums[idx]);
-        int subtract = solve(nums, idx + 1, target + nums[idx]);
-
-        return dp[idx][target + offset] = add + subtract;
     }
 }
