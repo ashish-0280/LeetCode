3cd2532f5416dee3c1b33e8ec63f9e57cb46d9a0
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-26 20:39:52 +0530

    Time: 43 ms (12.17%), Space: 46.7 MB (5.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,23 @@
 class Solution {
+    Integer dp[][];
     public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length+1][amount+1];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
-        }
-        int ans = solve(coins, amount, coins.length-1, dp);
-        return ans == Integer.MAX_VALUE-1 ? -1 : ans;
+        dp = new Integer[coins.length+1][amount+1];
+        int ans = solve(coins, 0, amount);
+        return ans == Integer.MAX_VALUE-10 ? -1 : ans;
     }
-    public int solve(int coins[], int amount, int idx, int dp[][]){
-        if(idx < 0){
-            return Integer.MAX_VALUE-1;
-        }
+    public int solve(int coins[], int idx, int amount){
         if(amount == 0){
             return 0;
         }
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
+        if(idx == coins.length || amount < 0){
+            return Integer.MAX_VALUE-10;
         }
+        if(dp[idx][amount] != null) return dp[idx][amount];
+        int take = Integer.MAX_VALUE;
         if(amount >= coins[idx]){
-            int ans1 = solve(coins, amount-coins[idx], idx, dp) + 1;
-            int ans2 = solve(coins, amount, idx-1, dp);
-            return dp[idx][amount] = Math.min(ans1, ans2);
-        } else {
-            return dp[idx][amount] = solve(coins, amount, idx-1, dp);
+            take = 1 + solve(coins, idx, amount-coins[idx]);
         }
+        int notTake = solve(coins, idx+1, amount);
+        return dp[idx][amount] = Math.min(take, notTake);
     }
 }
