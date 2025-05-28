149b2756418dc5b8f0b371a160a4284569ef2cf4
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-28 08:13:51 +0530

    Time: 15 ms (24.93%), Space: 43.1 MB (79.89%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
+        int dp[] = new int[nums.length];
         Arrays.sort(nums);
-
-        int[] dp = new int[n]; 
-        int[] prev = new int[n];     
-
         Arrays.fill(dp, 1);
-        Arrays.fill(prev, -1);
-
-        int maxIdx = 0;
-
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]){
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+        int hash[] = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for(int i=0; i<nums.length; i++){
+            hash[i] = i;
+            for(int prev_idx=0; prev_idx<=i-1; prev_idx++){
+                if(nums[i]%nums[prev_idx] == 0 && dp[i] < 1+dp[prev_idx]){
+                    dp[i] = 1+dp[prev_idx];
+                    hash[i] = prev_idx;
                 }
             }
-            if(dp[i] > dp[maxIdx]){
-                maxIdx = i;
+        }
+        int max = Integer.MIN_VALUE; int idx = 0;
+        for(int i=0; i<dp.length; i++){
+            if(max < dp[i]){
+                max = dp[i];
+                idx = i;
             }
         }
-
-        List<Integer> result = new ArrayList<>();
-        int idx = maxIdx;
-
-        while(idx >= 0){
-            result.add(nums[idx]);
-            idx = prev[idx];
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[idx]);
+        int lastIndex = idx;
+        while(hash[lastIndex] != lastIndex){
+            lastIndex = hash[lastIndex];
+            list.add(nums[lastIndex]);
         }
-
-        return result;
+        return list;
     }
 }
