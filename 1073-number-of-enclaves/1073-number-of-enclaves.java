f3fbb51b89e6937624d15eef74ff57efecb98b66
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-30 08:50:49 +0530

    Time: 694 ms (5.13%), Space: 55.2 MB (68.53%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,34 +1,54 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int m = grid.length; int n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         boolean vis[][] = new boolean[m][n];
-        int total = 0;
+        Queue <int[]> q = new LinkedList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    int num[] = new int[1];
-                    if(dfs(grid, i, j, vis, num)){
-                        total += num[0];
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(grid[i][j] == 1){
+                        grid[i][j] = 2;
+                        q.offer(new int[]{i, j});
+                        vis[i][j] = true;
                     }
                 }
             }
         }
-        return total;
-    }
-    public boolean dfs(int grid[][], int i, int j, boolean vis[][], int num[]){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length){
-            return false;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(grid[i][j]+" ");
+            }
+            System.out.println();
         }
-        if(grid[i][j] == 0 || vis[i][j]){
-            return true;
-        }
-        vis[i][j] = true;
-        num[0]++;
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0, 0, 1, -1};
 
-        boolean b1 = dfs(grid, i+1, j, vis, num);
-        boolean b2 = dfs(grid, i, j+1, vis, num);
-        boolean b3 = dfs(grid, i, j-1, vis, num);
-        boolean b4 = dfs(grid, i-1, j, vis, num);
-        return (b1 && b2 && b3 && b4);
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int cell[] = q.poll();
+                for(int j=0; j<4; j++){
+                    int x = cell[0] + dx[j];
+                    int y = cell[1] + dy[j];
+                    if(x>=0 && y>=0 && x<=m-1 && y<=n-1 && !vis[x][y]){
+                        if(grid[x][y] == 1){
+                            grid[x][y] = 2;
+                            q.offer(new int[]{x,y});
+                            vis[x][y] = true;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
