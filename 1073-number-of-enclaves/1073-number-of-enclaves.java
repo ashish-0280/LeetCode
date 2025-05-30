2302f0e223f89feb2283ebe883c56e1e4d59ec4e
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-30 08:54:36 +0530

    Time: 768 ms (5.13%), Space: 55.8 MB (65.04%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -2,7 +2,6 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean vis[][] = new boolean[m][n];
         Queue <int[]> q = new LinkedList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -10,7 +9,6 @@ class Solution {
                     if(grid[i][j] == 1){
                         grid[i][j] = 2;
                         q.offer(new int[]{i, j});
-                        vis[i][j] = true;
                     }
                 }
             }
@@ -26,21 +24,17 @@ class Solution {
 
         int cnt = 0;
         while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                int cell[] = q.poll();
+            int cell[] = q.poll();
                 for(int j=0; j<4; j++){
                     int x = cell[0] + dx[j];
                     int y = cell[1] + dy[j];
-                    if(x>=0 && y>=0 && x<=m-1 && y<=n-1 && !vis[x][y]){
+                    if(x>=0 && y>=0 && x<=m-1 && y<=n-1){
                         if(grid[x][y] == 1){
                             grid[x][y] = 2;
                             q.offer(new int[]{x,y});
-                            vis[x][y] = true;
                         }
                     }
                 }
-            }
         }
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
