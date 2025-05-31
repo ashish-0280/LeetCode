c3e13cb231de1ceb669ac7eefc59e8f98cc872f8
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-05-31 13:54:42 +0530

    Time: 61 ms (29.62%), Space: 45.2 MB (78.99%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -1,8 +1,6 @@
 class Solution {
     class Cell{
-        int x;
-        int y;
-        int distance;
+        int x; int y; int distance;
         public Cell(int x, int y, int distance){
             this.x = x;
             this.y = y;
@@ -12,34 +10,31 @@ class Solution {
     public int minimumEffortPath(int[][] heights) {
         int m = heights.length;
         int n = heights[0].length;
-        int dx[] = {0, 0, -1, 1};
-        int dy[] = {1, -1, 0, 0};
-        PriorityQueue<Cell> q = new PriorityQueue<>(Comparator.comparingInt(a -> a.distance));
-        q.offer(new Cell(0, 0, 0));
+
+        PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.distance));
+        pq.add(new Cell(0, 0, 0));
         int result[][] = new int[m][n];
-        for(int row[]: result){
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
+        for(int row[]: result) Arrays.fill(row, Integer.MAX_VALUE);
         result[0][0] = 0;
-        int max = Integer.MIN_VALUE;
-        while(!q.isEmpty()){
-            Cell cell = q.poll();
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+        while(!pq.isEmpty()){
+            Cell cell = pq.poll();
             int x = cell.x; int y = cell.y; int distance = cell.distance;
             for(int i=0; i<4; i++){
-                int newX = x + dx[i]; int newY = y + dy[i];
-                
-                if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
-                    int newDis = Math.abs(heights[newX][newY] - heights[x][y]);
-                    int maxDis = Math.max(distance, newDis);
-                    if(maxDis < result[newX][newY]){
-                        result[newX][newY] = maxDis;
-                        q.offer(new Cell(newX, newY, maxDis));
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if(newX >= 0 && newY >= 0 && newX < m && newY < n){
+                    int currDistance = Math.abs(heights[x][y] - heights[newX][newY]);
+                    int maxDistance = Math.max(currDistance, distance);
+                    if(maxDistance < result[newX][newY]){
+                        result[newX][newY] = maxDistance;
+                        pq.offer(new Cell(newX, newY, maxDistance));
                     }
                 }
             }
         }
-        int ans = result[m-1][n-1];
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-
+        return result[m-1][n-1] == Integer.MAX_VALUE ? -1 : result[m-1][n-1];
     }
 }
