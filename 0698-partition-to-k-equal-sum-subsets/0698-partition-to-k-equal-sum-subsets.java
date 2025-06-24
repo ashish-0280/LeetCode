90589aa0031a531847c0359fee5e1e95a31fbbe5
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-06-24 16:16:21 +0530

    Time: 24 ms (68.76%), Space: 44.7 MB (18.03%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -8,17 +8,17 @@ class Solution {
         int target = sum/k;
         Arrays.sort(nums);
         Boolean dp[] = new Boolean[1<<n];
-        return solve(nums, (1<<n)-1, 0, target, dp);   
+        return solve(nums, 0, 0, target, dp);   
     }
     public boolean solve(int nums[], int mask, int currSum, int target, Boolean dp[]){
-        if(mask == 0) return true;
+        if(mask == (1<<nums.length)-1) return true;
         if(dp[mask] != null) return dp[mask];
         dp[mask] = false;
         for(int i=0; i<nums.length; i++){
-            if((mask & (1<<i)) != 0){
+            if((mask & (1<<i)) == 0){
                 int newSum = nums[i] + currSum;
                 if(newSum > target) continue;
-                int newMask = mask ^ (1<<i);
+                int newMask = mask | (1<<i);
                 if(solve(nums, newMask, newSum%target, target, dp)){
                     return dp[mask] = true;
                 }
