d4266850569c3b36659598b02ed743e707606eba
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-04 09:57:41 +0530

    Time: 1 ms (100%), Space: 43.4 MB (29.27%) - LeetHub

--- 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.java
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public char kthCharacter(long k, int[] operations) {
-        if(k == 1) return 'a';
-        long length = 1;
-        int len = operations.length; long idx = 0;
-        int operation = operations[0];
-        for(int i=0; i<len; i++){
-            length = length*2;
-            if(length >= k){
-                operation = operations[i];
-                idx = k-(length/2);
-                break;
-            }
+       if(k==1){
+        return 'a';
+       }
+       long len = 1;
+       long newk = 0;
+       int type = 0;
+       int n = operations.length;
+       for(int i =0; i<operations.length; i++){
+        len*=2;
+        if(len>=k){
+            type = operations[i];
+            newk = k-(len/2);
+            break;
         }
-        char ch = kthCharacter(idx, operations);
-        if(operation == 0){
-            return ch;
-        } else {
-            if(ch == 'z') {
-                return 'a';
-            } else {
-                return ++ch;
-            }
-        }
+       }
+       char c = kthCharacter(newk,operations);
+       if(c=='z'){
+        return 'a';
+       }
+       if(type == 0){
+        return c;
+       }
+       return (char)(c+1);
     }
 }
