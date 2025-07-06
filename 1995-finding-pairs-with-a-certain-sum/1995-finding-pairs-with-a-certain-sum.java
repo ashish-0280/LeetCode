9763b886eef03072dbb0182e0eeee75b329116bf
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-06 08:42:36 +0530

    Time: 141 ms (80%), Space: 74.4 MB (64.56%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,40 +1,41 @@
-import java.util.*;
-
 class FindSumPairs {
-    private int[] nums1, nums2;
-    private Map<Integer, Integer> freqMap;
-
+    int nums1[]; int nums2[];
+    Map<Integer, Integer> map;
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1 = nums1;
         this.nums2 = nums2;
 
-        freqMap = new HashMap<>();
-        for (int num : nums2) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        map = new HashMap<>();
+        for(int num: nums2){
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
     }
-
+    
     public void add(int index, int val) {
-        int oldVal = nums2[index];
-        int newVal = oldVal + val;
+        int oldValue = nums2[index];
+        int newValue = oldValue + val;
 
-        // Update frequency map
-        freqMap.put(oldVal, freqMap.get(oldVal) - 1);
-        if (freqMap.get(oldVal) == 0) {
-            freqMap.remove(oldVal);
+        map.put(oldValue, map.get(oldValue)-1);
+        if(map.get(oldValue) == 0){
+            map.remove(oldValue);
         }
-
-        freqMap.put(newVal, freqMap.getOrDefault(newVal, 0) + 1);
-
-        nums2[index] = newVal;
+        map.put(newValue, map.getOrDefault(newValue, 0)+1);
+        nums2[index] = newValue;
     }
-
+    
     public int count(int tot) {
         int count = 0;
-        for (int num : nums1) {
-            int complement = tot - num;
-            count += freqMap.getOrDefault(complement, 0);
+        for(int num: nums1){
+            int needed = tot - num;
+            count += map.getOrDefault(needed, 0);
         }
         return count;
     }
 }
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
