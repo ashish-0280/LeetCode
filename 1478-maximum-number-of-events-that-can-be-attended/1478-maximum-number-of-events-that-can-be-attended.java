65af41c75d1cfd6521eda7ea6fa9cb12f272c254
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-07 07:59:45 +0530

    Time: 64 ms (89.04%), Space: 75.3 MB (92.07%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a,b) -> a[0]-b[0]);
+        Arrays.sort(events, (a,b) -> a[0] - b[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int i=0; int cnt = 0; int n = events.length;
 
+        int i=0; int cnt = 0; int n = events.length;
         int max = 0;
         for(int row[]: events){
-            max = Math.max(max, row[1]);
+            max = Math.max(row[1], max);
         }
 
-        for(int day = 1; day<=max; day++){
-            while(i<n && events[i][0] == day){
-                pq.offer(events[i][1]);
+        for(int day = 1; day <= max; day++){
+            while(i<n && day == events[i][0]){
+                pq.add(events[i][1]);
                 i++;
             }
             while(!pq.isEmpty() && pq.peek() < day){
                 pq.poll();
             }
+
             if(!pq.isEmpty()){
                 pq.poll();
                 cnt++;
