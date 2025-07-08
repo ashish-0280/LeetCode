a566f3f9f48a481c2d5ebe78d19e44d8eaf3363e
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-08 20:12:16 +0530

    Time: 0 ms (100%), Space: 41.2 MB (35.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,18 @@
 class Solution {
-    Integer dp[];
     public int rob(int[] nums) {
-        dp = new Integer[nums.length];
-        return solve(nums, 0);
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, dp);
     }
-    public int solve(int nums[], int idx){
+    public int solve(int nums[], int idx, int dp[]){
         if(idx >= nums.length){
             return 0;
         }
-        if(dp[idx] != null){
-            return dp[idx];
-        }
-        int include = nums[idx] + solve(nums, idx+2);
-        int exclude = solve(nums, idx+1);
+        if(dp[idx] != -1) return dp[idx];
+        int include = nums[idx] + solve(nums, idx+2, dp);
+        int exclude = solve(nums, idx+1, dp);
+
         return dp[idx] = Math.max(include, exclude);
     }
 }
