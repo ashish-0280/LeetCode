89d428868c96fcc8320eb24ac4ca041ae4bd2b3d
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-08 08:02:30 +0530

    Time: 125 ms (25.48%), Space: 133.6 MB (51.44%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,44 +1,37 @@
 class Solution {
-    int[][] memo;
-    int[][] events;
-    
+    int events[][];
+    int dp[][];
     public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(events, (a,b) -> a[0]-b[0]);
         this.events = events;
-        int n = events.length;
-        memo = new int[n][k + 1];
-        for (int[] row : memo) {
+        dp = new int[events.length][k+1];
+        for(int row[]: dp){
             Arrays.fill(row, -1);
         }
-        return dfs(0, k);
+        return solve(0, k);
     }
-    
-    private int dfs(int index, int k) {
-        if (index == events.length || k == 0) return 0;
-        
-        if (memo[index][k] != -1) return memo[index][k];
-        
-        int skip = dfs(index + 1, k);
-        
-        // Option 2 → attend current event
-        int nextIndex = findNext(index);
-        int take = events[index][2] + dfs(nextIndex, k - 1);
-        
-        return memo[index][k] = Math.max(skip, take);
+    public int solve(int idx, int k){
+        if(idx >= events.length || k==0){
+            return 0;
+        }
+        if(dp[idx][k] != -1){
+            return dp[idx][k];
+        }
+        int exclude = solve(idx+1, k);
+        int nextIndex = findIndex(idx);
+        int include = events[idx][2] + solve(nextIndex, k-1);
+        return dp[idx][k] = Math.max(include, exclude);
     }
-    
-    private int findNext(int index) {
-        int lo = index + 1;
-        int hi = events.length - 1;
-        int ans = events.length;
-        int currEnd = events[index][1];
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (events[mid][0] > currEnd) {
+    public int findIndex(int idx){
+        int endDay = events[idx][1];
+        int i=0; int j=events.length-1; int ans = j+1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(events[mid][0] > endDay){
                 ans = mid;
-                hi = mid - 1;
+                j = mid-1;
             } else {
-                lo = mid + 1;
+                i=mid+1;
             }
         }
         return ans;
