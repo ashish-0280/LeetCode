031541ba09e19bb04d7a699718f2ce40f563f150
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-11 17:37:26 +0530

    Time: 43 ms (10.66%), Space: 45.2 MB (14.45%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,53 +1,23 @@
 class Solution {
     public int numberOfSubstrings(String s) {
+        Set<Character> set = new HashSet<>();
+        set.add('a'); set.add('b'); set.add('c');
+        int i=0; int j=0;
+        Map<Character, Integer> map = new HashMap<>();
         int cnt = 0;
-        if(s.length() < 3){
-            return 0;
-        }
-        int arr[] = new int[3];
-        int j = 0; int i = 0;
-        while(j<3){
-            if(s.charAt(j) == 'a'){
-                arr[0]++;
-            } else if(s.charAt(j) == 'b'){
-                arr[1]++;
-            } else {
-                arr[2]++;
+        while(j<s.length()){
+            char ch = s.charAt(j);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+            while(map.size() == set.size()){
+                map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                if(map.get(s.charAt(i)) == 0){
+                    map.remove(s.charAt(i));
+                }
+                cnt += s.length()-j;
+                i++; 
             }
             j++;
         }
-        while(i < s.length()-2){
-            while(j<=s.length()){
-                if(check(arr)){
-                    cnt += s.length()-j+1;
-                    break;
-                }
-                if(j==s.length()) break;
-                char ch = s.charAt(j);
-                if(ch == 'a'){
-                    arr[0]++;
-                } else if(ch == 'b'){
-                    arr[1]++;
-                } else {
-                    arr[2]++;
-                }
-                j++;
-            }
-            if(s.charAt(i) == 'a'){
-                arr[0]--;
-            } else if(s.charAt(i) == 'b'){
-                arr[1]--;
-            } else {
-                arr[2]--;
-            }
-            i++;
-        }
         return cnt;
-    }
-    public boolean check(int arr[]){
-        if(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1){
-            return true;
-        }
-        return false;
     }
 }
