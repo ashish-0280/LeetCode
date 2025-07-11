b609ba9f5dadad198d29776dc845ed78a41aab2c
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-11 10:05:20 +0530

    Time: 56 ms (86.94%), Space: 100.9 MB (28.12%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.java
@@ -1,58 +1,44 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        int[] meetingCounts = new int[n];
-        long[] endTimes = new long[n];
-        
-        // Sort meetings by start time
+        int meetingCount[] = new int[n];
+        long endTimes[] = new long[n];
+
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        for (int[] meeting : meetings) {
+        for(int meeting[]: meetings){
             int start = meeting[0];
             int end = meeting[1];
             int duration = end - start;
-            
-            // Find rooms that are free before the meeting starts
-            int freeRoom = -1;
-            for (int i = 0; i < n; i++) {
-                if (endTimes[i] <= start) {
-                    freeRoom = i;
+
+            int free = -1;
+            for(int i=0; i<n; i++){
+                if(endTimes[i] <= start){
+                    free = i;
                     break;
                 }
             }
-            
-            if (freeRoom != -1) {
-                // Room available: schedule the meeting in freeRoom
-                endTimes[freeRoom] = start + duration;
-                meetingCounts[freeRoom]++;
+            if(free != -1){
+                endTimes[free] = start + duration;
+                meetingCount[free]++;
             } else {
-                // No rooms free: find earliest available room
-                int earliestRoom = 0;
-                long earliestEndTime = endTimes[0];
-                
-                for (int i = 1; i < n; i++) {
-                    if (endTimes[i] < earliestEndTime) {
-                        earliestEndTime = endTimes[i];
-                        earliestRoom = i;
+                int earlyRoom = 0;
+                long time = endTimes[0];
+                for(int i=1; i<n; i++){
+                    if(endTimes[i] < time){
+                        time = endTimes[i];
+                        earlyRoom = i;
                     }
                 }
-                
-                // Delay meeting until earliest room is free
-                endTimes[earliestRoom] = earliestEndTime + duration;
-                meetingCounts[earliestRoom]++;
+                endTimes[earlyRoom] = time + duration;
+                meetingCount[earlyRoom]++;
             }
         }
-        
-        // Find the room with the most meetings
-        int maxMeetings = 0;
-        int roomWithMaxMeetings = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if (meetingCounts[i] > maxMeetings) {
-                maxMeetings = meetingCounts[i];
-                roomWithMaxMeetings = i;
+        int max = 0; int room = 0;
+        for(int i=0; i<n; i++){
+            if(meetingCount[i] > max){
+                max = meetingCount[i];
+                room = i;
             }
         }
-        
-        return roomWithMaxMeetings;
+        return room;
     }
 }
