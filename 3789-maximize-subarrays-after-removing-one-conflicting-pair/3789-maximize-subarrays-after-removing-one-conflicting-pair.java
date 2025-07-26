d29a2f645bd299b94831c0195a80513db9f6a06c
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-07-26 08:09:14 +0530

    Time: 432 ms (7.69%), Space: 171.1 MB (5.13%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
@@ -1,7 +1,7 @@
 class Solution {
-    static class P {
+    static class Pair {
         int r, i;
-        P(int r, int i) {
+        Pair(int r, int i) {
             this.r = r;
             this.i = i;
         }
@@ -9,39 +9,39 @@ class Solution {
 
     public long maxSubarrays(int n, int[][] pairs) {
         int m = pairs.length;
-        List<List<P>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
+        List<List<Pair>> starts = new ArrayList<>();
+        for (int i = 0; i <= n; i++) starts.add(new ArrayList<>());
 
         for (int i = 0; i < m; i++) {
-            int a = pairs[i][0];
-            int b = pairs[i][1];
-            int l = Math.min(a, b);
-            int r = Math.max(a, b);
-            list.get(l).add(new P(r, i));
+            int a = pairs[i][0], b = pairs[i][1];
+            int l = Math.min(a, b), r = Math.max(a, b);
+            starts.get(l).add(new Pair(r, i));
         }
 
-        TreeSet<P> set = new TreeSet<>((a, b) -> (a.r != b.r) ? Integer.compare(a.r, b.r) : Integer.compare(a.i, b.i));
+        TreeSet<Pair> active = new TreeSet<>((a, b) ->
+            (a.r != b.r) ? Integer.compare(a.r, b.r) : Integer.compare(a.i, b.i)
+        );
 
         int[] freq = new int[n + 2];
         long base = 0;
         long[] gain = new long[m];
 
         for (int i = n; i >= 1; i--) {
-            for (P p : list.get(i)) {
-                set.add(p);
+            for (Pair p : starts.get(i)) {
+                active.add(p);
                 freq[p.r]++;
             }
 
-            if (set.isEmpty()) {
+            if (active.isEmpty()) {
                 base += n + 1 - i;
             } else {
-                P p = set.first();
+                Pair p = active.first();
                 int minR = p.r;
                 base += minR - i;
 
                 if (freq[minR] == 1) {
                     int idx = p.i;
-                    P next = set.higher(new P(minR, Integer.MAX_VALUE));
+                    Pair next = active.higher(new Pair(minR, Integer.MAX_VALUE));
                     int nextR = (next == null) ? (n + 1) : next.r;
                     gain[idx] += nextR - minR;
                 }
