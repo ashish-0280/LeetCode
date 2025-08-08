d53989ae36bccc3f3b6a00f29949fa664832ca47
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-08-08 12:21:35 +0530

    Time: 186 ms (39.42%), Space: 79.4 MB (91.35%) - LeetHub

---
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+class Solution {
+    Integer[][][] dp;
+    
+    public int maximumAmount(int[][] coins) {
+        int m = coins.length;
+        int n = coins[0].length;
+        dp = new Integer[m][n][3];
+        
+        int result = solve(coins, 0, 0, 2);
+        
+        if (result <= Integer.MIN_VALUE / 2) {
+            return -1;
+        }
+        
+        return result;
+    }
+    
+    public int solve(int[][] coins, int i, int j, int k) {
+        int m = coins.length;
+        int n = coins[0].length;
+        
+        if (i >= m || j >= n) {
+            return Integer.MIN_VALUE / 2;
+        }
+        
+        if (i == m - 1 && j == n - 1) {
+            if (coins[i][j] < 0 && k > 0) {
+                return Math.max(coins[i][j], 0);
+            } else {
+                return coins[i][j];
+            }
+        }
+        
+        if (dp[i][j][k] != null) {
+            return dp[i][j][k];
+        }
+        
+        int ans;
+        
+        int valueWithCoin = coins[i][j] + Math.max(solve(coins, i + 1, j, k), solve(coins, i, j + 1, k));
+        
+        if (coins[i][j] < 0 && k > 0) {
+            int valueWithoutCoin = Math.max(solve(coins, i + 1, j, k - 1), solve(coins, i, j + 1, k - 1));
+            ans = Math.max(valueWithCoin, valueWithoutCoin);
+        } else {
+            ans = valueWithCoin;
+        }
+        
+        return dp[i][j][k] = ans;
+    }
+}
