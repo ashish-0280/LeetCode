d281d7eefd403bf9d94a9462be742a38153bb854
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-08-13 14:54:23 +0530

    Time: 4 ms (98.97%), Space: 42.7 MB (79.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.contains(s1)) return true;
-        int n = s1.length();
-        for(int i=0; i<s2.length()-n+1; i++){
-            if(check(s1, s2.substring(i, i+n))) return true;
+        if(s1.length() > s2.length()) return false;
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        for(int i=0; i<s1.length(); i++){
+            freq1[s1.charAt(i) - 'a']++;
+        }
+        int window = s1.length();
+        for(int i=0; i<window; i++){
+            freq2[s2.charAt(i) - 'a']++;
+        }
+        if(isEqual(freq1, freq2)) return true;
+
+        for(int i=window; i<s2.length(); i++){
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - window) - 'a']--;
+            if(isEqual(freq1, freq2)) return true;
         }
         return false;
     }
-    public boolean check(String s1, String s2){
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for(int i=0; i<s1.length(); i++){
-           freq1[s1.charAt(i)-'a']++;
-           freq2[s2.charAt(i)-'a']++;
-        }
+    public boolean isEqual(int arr1[], int arr2[]){
         for(int i=0; i<26; i++){
-            if(freq1[i] != freq2[i]) return false;
-        } 
+            if(arr1[i] != arr2[i]) return false;
+        }
         return true;
     }
 }
