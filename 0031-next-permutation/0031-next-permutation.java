07a7d18268c57992316d285086a912ef809774dc
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-08-15 14:15:22 +0530

    Time: 0 ms (100%), Space: 43.1 MB (56.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -26,7 +26,7 @@ class Solution {
         if(idx1 >= idx2){
             return;
         }
-        while(idx1 <= idx2){
+        while(idx1 < idx2){
             int temp = nums[idx1];
             nums[idx1] = nums[idx2];
             nums[idx2] = temp;
