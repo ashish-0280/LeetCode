6bd1dbe0f2bb585ddb95f9f27bda744f321ddec4
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-08-20 08:27:00 +0530

    Time: 10 ms (10.45%), Space: 55.2 MB (53.73%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,16 +1,36 @@
 class Solution {
+    int[][] memo;
+    int[][] matrix;
+    int m, n;
+    
     public int countSquares(int[][] matrix) {
-        int ans = 0;
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (matrix[i][j] != 0) {
-                    if(i != 0 && j != 0){
-                        matrix[i][j] = Math.min(matrix[i - 1][j - 1], Math.min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
-                    }
-                }
-                ans += matrix[i][j];
+        this.matrix = matrix;
+        m = matrix.length;
+        n = matrix[0].length;
+        memo = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        
+        int total = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += dfs(i, j);
             }
         }
-        return ans;
+        return total;
+    }
+    
+    private int dfs(int i, int j) {
+        if (i < 0 || j < 0) return 0;
+        if (matrix[i][j] == 0) return 0;
+        if (memo[i][j] != -1) return memo[i][j];
+        
+            memo[i][j] = 1 + Math.min(
+                Math.min(dfs(i-1, j), dfs(i, j-1)),
+                dfs(i-1, j-1)
+            );
+        return memo[i][j];
     }
 }
