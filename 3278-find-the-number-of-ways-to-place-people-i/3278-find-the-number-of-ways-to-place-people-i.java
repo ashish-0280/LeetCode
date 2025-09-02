6354d66175b727f3528c0072c9c536027fc70a74
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-02 09:43:34 +0530

    Time: 5 ms (100%), Space: 45 MB (10.45%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
@@ -4,16 +4,14 @@ class Solution {
             b[1] - a[1] : a[0] - b[0]);
         int cnt = 0;
         for (int i = 0; i < points.length; i++) {
-            int x0 = points[i][0], y0 = points[i][1];
-            int bot = Integer.MIN_VALUE, top = y0;
+            int x1 = points[i][0], y1 = points[i][1];
+            int bound = Integer.MIN_VALUE, top = y1;
             for (int j = i + 1; j < points.length; j++) {
-                int x1 = points[j][0], y1 = points[j][1];
-                if (y1 <= top && y1 > bot) {
-                    // found a valid rectangular.
+                int x2 = points[j][0], y2 = points[j][1];
+                if (y2 <= top && y2 > bound) {
                     cnt++;
-                    bot = y1;
-                    // to avoid points on the same y value;
-                    if (y1 == top) top--;
+                    bound = y2;
+                    if (y2 == top) top--;
                 }
             }
         }
