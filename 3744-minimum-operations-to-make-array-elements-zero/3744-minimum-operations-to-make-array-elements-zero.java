eb2791fdd579a981b29a693cc47b453cc1097d2b
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-06 07:21:40 +0530

    Time: 16 ms (72.22%), Space: 101.2 MB (58.33%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
@@ -2,31 +2,25 @@ class Solution {
     public long minOperations(int[][] queries) {
         long ans = 0;
         for(int query[]: queries){
-            ans += solve(query[0], query[1]);
+            long steps = solve(query[0], query[1]);
+            ans += (steps+1)/2;
         }
         return ans;
     }
     public long solve(int l, int r){
-        long S = 0;
-        long tmax = 0;
-        
-        // k is number of divisions needed
-        for (int k = 1; ; k++) {
-            long low = (long) Math.pow(4, k - 1);
-            long high = (long) Math.pow(4, k) - 1;
-            if (low > r) break; // no more numbers in this range
-            
+        long sum = 0;
+        long low = 1; long k = 1; 
+        while(k > 0){
+            if(low > r) break;
+            long high = 4*low - 1;
             long start = Math.max(l, low);
             long end = Math.min(r, high);
-            
-            if (start <= end) {
-                long count = end - start + 1;
-                S += count * k;
-                tmax = k;
+            if(start <= end){
+                sum += (end - start + 1) * k;
             }
+            k++;
+            low *= 4;
         }
-        
-        long ceilHalf = ((S + 1) >> 1); // ceil(S/2)
-        return Math.max(tmax, ceilHalf);
+        return sum;
     }
 }
