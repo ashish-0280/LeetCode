fa7fffa3f2afd5abd3bbfab5eb650b72b8210fb4
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-08 08:47:25 +0530

    Time: 40 ms (100%), Space: 56.1 MB (100%) - LeetHub

--- 4000-count-bowl-subarrays/4000-count-bowl-subarrays.java
+++ 4000-count-bowl-subarrays/4000-count-bowl-subarrays.java
@@ -1,21 +1,74 @@
-class Solution {
-    public long bowlSubarrays(int[] nums) {
-        long count = 0;
-        Stack<Integer> stk = new Stack<>();
+class Solution 
+{
+    public long bowlSubarrays(int[] nums) 
+    {
+        long count=0;
+        
+        //FIND NEXT GREATER
+        int nextgreater[]=new int[nums.length];
+        nextgreater=nextgreaterelement(nums);
 
-        for (int right = 0; right < nums.length; right++) {
-            while (!stk.isEmpty() && nums[stk.peek()] <= nums[right]) {
-                int left = stk.pop();
-                if (right - left + 1 >= 3) {
-                    count++;
-                }
-            }
-            if (!stk.isEmpty() && right - stk.peek() + 1 >= 3) {
-                count++;
-            }
-            stk.push(right);
+        //FIND PREV GREATER
+        int prevgreater[]=new int[nums.length];
+        prevgreater=prevgreaterelement(nums);
+
+        //NOW TRVAERSE FROM START -> use next greater
+        for(int i=0;i<nums.length;i++)
+        {
+            int next=nextgreater[i];
+
+            if(next!=-1 && next-i+1>=3){count++;}
+        }
+
+        //NOW TRAVERSE AGAIN FROM START -> use prev greater
+        for(int i=0;i<nums.length;i++)
+        {
+            int prev=prevgreater[i];
+
+            if(prev!=-1 && i-prev+1>=3){count++;}
         }
 
         return count;
     }
+
+    public int[] nextgreaterelement(int nums[])
+    {
+        Deque<Integer> stack=new ArrayDeque<>();
+        int res[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            int curr=nums[i];
+
+            while(!stack.isEmpty() && nums[stack.peek()]<=curr)
+            {
+                stack.pop();
+            }
+
+            res[i]=(stack.isEmpty()) ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public int[] prevgreaterelement(int nums[])
+    {
+        Deque<Integer> stack=new ArrayDeque<>();
+        int res[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            int curr=nums[i];
+
+            while(!stack.isEmpty() && nums[stack.peek()]<=curr)
+            {
+                stack.pop();
+            }
+
+            res[i]=(stack.isEmpty()) ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        return res;
+    }
+
 }
