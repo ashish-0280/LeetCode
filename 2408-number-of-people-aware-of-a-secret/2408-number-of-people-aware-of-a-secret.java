acced67b27f7b4911c3b67ab21150257f57ea68b
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-09 09:13:33 +0530

    Time: 20 ms (11.82%), Space: 40.9 MB (60%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.java
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.java
@@ -1,22 +1,23 @@
 class Solution {
+    int MOD = 1000000007;
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        int MOD = 1_000_000_007;
+        
         long[] dp = new long[n + 1];
         dp[1] = 1;
 
-        for (int day = 2; day <= n; day++) {
-            for (int i = delay; i < forget; i++) {
-                if (day - i > 0) {
-                    dp[day] = (dp[day] + dp[day - i]) % MOD;
+        for (int i = 2; i <= n; i++) {
+            for (int j = delay; j < forget; j++) {
+                if (i - j > 0) {
+                    dp[i] = (dp[i] + dp[i - j]) % MOD;
                 }
             }
         }
 
-        long res = 0;
-        for (int i = n - forget + 1; i <= n; i++) {
-            if (i > 0) res = (res + dp[i]) % MOD;
+        long ans = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i >= n-forget+1) ans = (ans + dp[i]) % MOD;
         }
 
-        return (int) res;
+        return (int) ans;
     }
 }
