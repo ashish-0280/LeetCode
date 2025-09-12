4f9e9456baab21040458529f2415277ffab1c8f3
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-12 07:33:02 +0530

    Time: 3 ms (96.06%), Space: 45.9 MB (8.66%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,26 +1,11 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int evenCount = 0;
-        int oddCount = 0; String vowels = "aeiou"; int i=0;
-        while(i<s.length()){
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(vowels.indexOf(ch) != -1){
-                int j = i;
-                while(i<s.length() && vowels.indexOf(s.charAt(i)) != -1){
-                    i++;
-                }
-                if(i-j % 2 == 0){
-                    evenCount++;
-                } else {
-                    oddCount++;
-                }
-                continue;
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
             }
-            i++;
         }
-        if(evenCount >= oddCount){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
