e7f07c990e0481efd974a5eef204310ebce672bb
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-14 22:27:54 +0530

    Time: 1697 ms (100%), Space: 47 MB (100%) - LeetHub

--- 3873-subsequence-sum-after-capping-elements/3873-subsequence-sum-after-capping-elements.java
+++ 3873-subsequence-sum-after-capping-elements/3873-subsequence-sum-after-capping-elements.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean[] subsequenceSumAfterCapping(int[] nums, int k) {
         int n = nums.length;
-        boolean[] result = new boolean[n];
         Arrays.sort(nums);
         Set<Integer> set = new HashSet<>();
         set.add(0);
         int idx = 0;
-        for (int i = 1; i <= n; i++) {
-            while (idx < n && nums[idx] <= i) {
-                List<Integer> current = new ArrayList<>(set);
-                for (int sum : current) {
-                    if(sum + nums[idx] <= k){
-                        set.add(sum+nums[idx]);
-                    }
+        boolean result[] = new boolean[n];
+        for(int i=1; i<=n; i++){
+            while(idx < n && nums[idx] <= i){
+                List<Integer> list = new ArrayList<>(set);
+                for(int sum: list){
+                    int newSum = sum + nums[idx];
+                    if(newSum <= k) set.add(newSum);
                 }
                 idx++;
             }
-            if (set.contains(k)) {
-                result[i - 1] = true;
+            if(set.contains(k)){
+                result[i-1] = true;
             } else {
                 for(int p=idx; p<n; p++){
-                    if(set.contains(k-(p-idx+1)*i)){ 
-                        result[i-1]=true; 
+                    int right = p-idx+1;
+                    int needed = k - (right * i);
+                    if(set.contains(needed)){
+                        result[i-1] = true;
                         break;
-                    } 
+                    }
                 }
             }
         }
