1f3e66a38a19df527efaadb778576caffabaa503
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-23 08:37:16 +0530

    Time: 9 ms (5.14%), Space: 42.1 MB (6.38%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String arr1[] = version1.split("\\.");
+        String arr2[] = version2.split("\\.");
+        for(int i=0; i<Math.max(arr1.length, arr2.length); i++){
+            int x = 0; int y = 0;
+            if(i < arr1.length){
+                x = Integer.parseInt(arr1[i]);
+            } 
+            if(i < arr2.length){
+                y = Integer.parseInt(arr2[i]);
+            } 
+            System.out.println(x + " " + y);
+            if(x > y) return 1;
+            if(x < y) return -1;
+        }
+        return 0;
+    }
+}
