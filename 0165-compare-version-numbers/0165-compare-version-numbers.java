0ea0f1ccaaa96799c479586a0fb9e37f2ba95c20
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-09-23 08:39:16 +0530

    Time: 9 ms (5.14%), Space: 42 MB (6.38%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -5,10 +5,10 @@ class Solution {
         for(int i=0; i<Math.max(arr1.length, arr2.length); i++){
             int x = 0; int y = 0;
             if(i < arr1.length){
-                x = Integer.parseInt(arr1[i]);
+                x = Integer.valueOf(arr1[i]);
             } 
             if(i < arr2.length){
-                y = Integer.parseInt(arr2[i]);
+                y = Integer.valueOf(arr2[i]);
             } 
             System.out.println(x + " " + y);
             if(x > y) return 1;
