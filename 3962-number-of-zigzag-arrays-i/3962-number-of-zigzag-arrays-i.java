495b3a072cc83e2e92c13cda2793b9e452cb5f27
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-10-02 11:08:04 +0530

    Time: 196 ms (98.54%), Space: 41.6 MB (99.69%) - LeetHub

--- 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.java
+++ 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.java
@@ -1,42 +1,28 @@
 class Solution {
-    public int zigZagArrays(int n, int l, int r) {
-        int mod = 1000000007;
-        int len = r - l + 1;
-
-        int[] inc = new int[len];  
-        int[] dec = new int[len]; 
-        int[] nxtInc = new int[len];
-        int[] nxtDec = new int[len];
-
-        for (int i = 0; i < len; i++) {
-            inc[i] = 1;
-            dec[i] = 1;
-            nxtInc[i] = 1;
-            nxtDec[i] = 1;
-        }
-
-        for (int step = 0; step < n; step++) {
-            for (int j = 0; j < len; j++) {
-                nxtInc[j] = dec[j];
-                nxtDec[j] = inc[j];
-            }
-
-            int prefInc = 0;
-            int prefDec = 0;
-
-            for (int j = 0; j < len; j++) {
-                prefInc = (prefInc + nxtInc[j]) % mod;
-                inc[j] = (prefInc - nxtInc[j] + mod) % mod;
-
-                prefDec = (prefDec + nxtDec[len - 1 - j]) % mod;
-                dec[len - 1 - j] = (prefDec - nxtDec[len - 1 - j] + mod) % mod;
+        public int zigZagArrays(int n, int l, int r) {
+        int MOD = 1_000_000_007;
+        r -= l;
+        int[] dp = new int[r + 1];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            int pre = 0, pre2;
+            if ((i & 1) == 1) {
+                for (int v = 0; v <= r; v++) {
+                    pre2 = pre + dp[v];
+                    dp[v] = pre;
+                    pre = pre2 % MOD;
+                }
+            } else {
+                for (int v = r; v >= 0; v--) {
+                    pre2 = pre + dp[v];
+                    dp[v] = pre;
+                    pre = pre2 % MOD;
+                }
             }
         }
-
-        int ans = 0;
-        for (int x : nxtInc) ans = (ans + x) % mod;
-        for (int x : nxtDec) ans = (ans + x) % mod;
-
-        return ans;
+        int res = 0;
+        for (int v : dp)
+            res = (res + v) % MOD;
+        return res * 2 % MOD;
     }
 }
