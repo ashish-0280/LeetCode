6eeae9e611f4dade9deec0c27d38135694f2193f
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-10-12 10:44:07 +0530

    Time: 300 ms (100%), Space: 70.9 MB (100%) - LeetHub

--- 4056-longest-balanced-substring-ii/4056-longest-balanced-substring-ii.java
+++ 4056-longest-balanced-substring-ii/4056-longest-balanced-substring-ii.java
@@ -1,63 +1,66 @@
 class Solution {
     public int longestBalanced(String s) {
         int n = s.length();
-        if (n == 0) return 0;
-
-        int maxLen = 1, runLen = 1;
-        for (int i = 1; i < n; i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) runLen++;
-            else {
-                maxLen = Math.max(maxLen, runLen);
-                runLen = 1;
+        int len = 1; int max = 1;
+        for(int i=1; i<n; i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                len++;
+                max = Math.max(max, len);
+            } else {
+                len = 1;
             }
         }
-        maxLen = Math.max(maxLen, runLen);
-
-        int cntA = 0, cntB = 0, cntC = 0;
-        Map<String, Integer> diffMap = new HashMap<>();
-        diffMap.put("0,0", -1);
-        for (int i = 0; i < n; i++) {
+        int a = 0, b = 0, c = 0;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("0,0", -1);
+        for(int i=0; i<n; i++){
             char ch = s.charAt(i);
-            if (ch == 'a') cntA++;
-            else if (ch == 'b') cntB++;
-            else if (ch == 'c') cntC++;
-
-            String key = (cntA - cntB) + "," + (cntA - cntC);
-            if (diffMap.containsKey(key)) maxLen = Math.max(maxLen, i - diffMap.get(key));
-            else diffMap.put(key, i);
+            if(ch == 'a'){
+                a++;
+            } else if(ch == 'b'){
+                b++;
+            } else {
+                c++;
+            }
+            int diff1 = a - b; int diff2 = a - c;
+            String key = diff1 + "," + diff2;
+            if(map.containsKey(key)){
+                max = Math.max(max, i-map.get(key));
+            } else {
+                map.put(key, i);
+            } 
         }
+        max = Math.max(max, solve(s, 'a', 'b', 'c'));
+        max = Math.max(max, solve(s, 'a', 'c', 'b'));
+        max = Math.max(max, solve(s, 'b', 'c', 'a'));
 
-        maxLen = Math.max(maxLen, maxPair(s, 'a', 'b', 'c'));
-        maxLen = Math.max(maxLen, maxPair(s, 'a', 'c', 'b'));
-        maxLen = Math.max(maxLen, maxPair(s, 'b', 'c', 'a'));
-
+        return max;
+    }
+    public int solve(String s, char c1, char c2, char skip){
+        int maxLen = 0; int n = s.length();
+        for(int i=0; i<s.length(); i++){
+            while(i < n && s.charAt(i) == skip) i++;
+            if(i >= n) break;
+            int p = i;
+            while(i < n && s.charAt(i) != skip) i++;
+            int q = i-1; int x = 0; int y = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, p-1);
+            for(int j=p; j<=q; j++){
+                char ch = s.charAt(j);
+                if (ch == c1){
+                    x++;
+                } else if(ch == c2){
+                    y++;
+                }
+                int key = x-y;
+                if(map.containsKey(key)){
+                    maxLen = Math.max(maxLen, j-map.get(key));
+                } else {
+                    map.put(key, j);
+                }
+            }
+        }
         return maxLen;
-    }
-
-    public int maxPair(String s, char x, char y, char skip) {
-        int n = s.length(), best = 0;
-        for (int i = 0; i < n; i++) {
-            while (i < n && s.charAt(i) == skip) i++;
-            if (i >= n) break;
-            int start = i;
-            while (i < n && s.charAt(i) != skip) i++;
-            best = Math.max(best, pairBalance(s, start, i - 1, x, y));
-        }
-        return best;
-    }
-
-    private int pairBalance(String s, int start, int end, char x, char y) {
-        Map<Integer, Integer> diffMap = new HashMap<>();
-        int diff = 0, best = 0;
-        diffMap.put(0, start - 1);
-
-        for (int i = start; i <= end; i++) {
-            char ch = s.charAt(i);
-            if (ch == x) diff++;
-            else if (ch == y) diff--;
-            if (diffMap.containsKey(diff)) best = Math.max(best, i - diffMap.get(diff));
-            else diffMap.put(diff, i);
-        }
-        return best;
     }
 }
