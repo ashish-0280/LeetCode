76e611674e2725b333679bafd1039018d8f6af4e
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-10-13 07:55:45 +0530

    Time: 5 ms (76.33%), Space: 46.2 MB (41.69%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -9,7 +9,6 @@ class Solution {
             graph.get(arr[0]).add(arr[1]);
             indegree[arr[1]]++;
         }
-        if(isCycle(graph, numCourses)) return new int[0];
         List<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<numCourses; i++){
@@ -17,8 +16,9 @@ class Solution {
                 q.add(i);
             }
         }
+        int count = 0;
         while(!q.isEmpty()){
-            int curr = q.poll();
+            int curr = q.poll(); count++;
             ans.add(curr);
             for(int num: graph.get(curr)){
                 indegree[num]--;
@@ -27,6 +27,7 @@ class Solution {
                 }
             }
         }
+        if(count < numCourses) return new int[0];
         Collections.reverse(ans);
         int arr[] = new int[ans.size()];
         int i=0;
@@ -35,32 +36,5 @@ class Solution {
             i++;
         }
         return arr;
-    }
-    public boolean isCycle(List<List<Integer>> graph, int n){
-        boolean rec[] = new boolean[n];
-        boolean vis[] = new boolean[n];
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                if(dfs(graph, n, vis, rec, i)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    public boolean dfs(List<List<Integer>> graph, int n, boolean vis[], boolean rec[], int curr){
-        vis[curr] = true;
-        rec[curr] = true;
-        for(int neighbour: graph.get(curr)){
-            if(!vis[neighbour]){
-                if(dfs(graph, n, vis, rec, neighbour)){
-                    return true;
-                }
-            } else {
-                if(rec[neighbour]) return true;
-            }
-        }
-        rec[curr] = false;
-        return false;
     }
 }
