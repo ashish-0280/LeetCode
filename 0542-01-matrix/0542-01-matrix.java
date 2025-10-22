57276092a433437c147b0e722c17de4018ef7ab6
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-10-22 14:49:16 +0530

    Time: 13 ms (83.75%), Space: 47.6 MB (35.44%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length; int n = mat[0].length;
-        int result[][] = new int[m][n];
-        Queue <int[]> q = new LinkedList<>();
-        int directions[][] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        int steps = 1;
+        int m = mat.length; 
+        int n = mat[0].length;
         boolean vis[][] = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0){
@@ -14,18 +12,22 @@ class Solution {
                 }
             }
         }
-
+        int result[][] = new int[m][n];
+        int steps = 1;
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
         while(!q.isEmpty()){
             int size = q.size();
             for(int i=0; i<size; i++){
                 int cell[] = q.poll();
-                int x = cell[0]; int y = cell[1];
-                for(int dir[]: directions){
-                    int nx = x + dir[0]; int ny = y + dir[1];
-                    if(nx>=0 && ny>=0 && nx<m && ny<n && !vis[nx][ny]){
-                        vis[nx][ny] = true;
-                        result[nx][ny] = steps;
-                        q.offer(new int[]{nx, ny});
+                int row = cell[0]; int col = cell[1];
+                for(int j=0; j<4; j++){
+                    int x = row + dx[j];
+                    int y = col + dy[j];
+                    if(x>=0 && y>=0 && x<m && y<n && !vis[x][y]){
+                        vis[x][y] = true;
+                        result[x][y] = steps;
+                        q.offer(new int[]{x, y});
                     }
                 }
             }
