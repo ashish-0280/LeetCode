b849bde8632648ee720c287b097cab575f0aa26c
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-11-03 21:36:43 +0530

    Time: 1 ms (100%), Space: 44.1 MB (59.62%) - LeetHub

--- 4048-minimum-time-to-complete-all-deliveries/4048-minimum-time-to-complete-all-deliveries.java
+++ 4048-minimum-time-to-complete-all-deliveries/4048-minimum-time-to-complete-all-deliveries.java
@@ -2,17 +2,19 @@ class Solution {
     public long minimumTime(int[] d, int[] r) {
         long low = 0;
         long high = (long)(d[0] + d[1]) * Math.max(r[0], r[1]);
+        long ans = 0;
         long l = lcm(r[0], r[1]);
 
-        while(low < high){
-            long mid = low + (high - low) / 2;
+        while(low <= high){
+            long mid = low + (high - low)/2;
             if(check(d, r, mid, l)){
-                high = mid;
+                ans = mid;
+                high = mid-1;
             } else {
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 
     public boolean check(int d[], int r[], long mid, long l){
