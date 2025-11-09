69d7eac87c33d67f7f72ec6a17ecad2898a23e95
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-11-09 20:58:40 +0530

    Time: 7 ms (0%), Space: 46.8 MB (0%) - LeetHub

--- 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minimumDistance(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        Map<Integer, TreeSet<Integer>> map2 = new HashMap<>();
+        Map<Integer, TreeSet<Integer>> indices = new HashMap<>();
 
         for(int i=0; i<nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(!map2.containsKey(nums[i])){
-                map2.put(nums[i], new TreeSet<>());
+            if(!indices.containsKey(nums[i])){
+                indices.put(nums[i], new TreeSet<>());
             }
-            map2.get(nums[i]).add(i);
+            indices.get(nums[i]).add(i);
         }
         int min = Integer.MAX_VALUE;
         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
             int val = entry.getValue(); int key = entry.getKey();
             if(val < 3) continue;
-            min = Math.min(min, minValue(map2.get(key)));
+            min = Math.min(min, minValue(indices.get(key)));
         }
         return min == Integer.MAX_VALUE ? -1 : min;
     }
     public int minValue(TreeSet<Integer> set) {
-        if (set.size() < 3) return -1; // not enough elements
-
         List<Integer> list = new ArrayList<>(set);
         int minCost = Integer.MAX_VALUE;
 
