95fe1ae877f80579c20d9fb8ffce40caf3e1da12
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-11-11 07:39:19 +0530

    Time: 88 ms (17.41%), Space: 99.1 MB (5.55%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,21 +1,21 @@
 class Solution {
     Integer dp[][][];
     public int findMaxForm(String[] strs, int m, int n) {
-        dp = new Integer[strs.length][m+101][n+101];
+        dp = new Integer[strs.length][m+1][n+1];
         return solve(strs, 0, m, n);
     }
     public int solve(String strs[], int idx, int m, int n){
         if(idx == strs.length){
             return 0;
         }
-        if(dp[idx][m+100][n+100] != null) return dp[idx][m+100][n+100];
+        if(dp[idx][m][n] != null) return dp[idx][m][n];
         int count[] = countZeroesOnes(strs[idx]);
         int include = 0;
         if(m >= count[0] && n >= count[1]){
             include = 1 + solve(strs, idx+1, m-count[0], n-count[1]);
         }
         int exclude = solve(strs, idx+1, m, n);
-        return dp[idx][m+100][n+100] = Math.max(include, exclude);
+        return dp[idx][m][n] = Math.max(include, exclude);
     }
     public int[] countZeroesOnes(String s){
         int cnt[] = new int[2];
