070eae8a778e068978bbf0de9654b018b0198296
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-11-21 08:26:07 +0530

    Time: 171 ms (79.63%), Space: 46.8 MB (30.09%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -2,12 +2,9 @@ class Solution {
     public int countPalindromicSubsequence(String s) {
         Set<Character> set = new HashSet<>();
         int n = s.length(); int ans = 0;
-        int i=0; int j = n-1;
-        while(i < n-2){
-            if(set.contains(s.charAt(i))) {
-                i++; continue;
-            }
-            j = n-1;
+        for(int i=0; i < n-2; i++){
+            if(set.contains(s.charAt(i))) continue;
+            int j = n-1;
             while(j > i + 1){
                 if(s.charAt(i) == s.charAt(j)){
                     set.add(s.charAt(i));
@@ -16,7 +13,6 @@ class Solution {
                 }
                 j--;
             }
-            i++;
         }
         return ans;
     }
