07e1bc337cb06eed10f8df2b57882e9ff965b6a5
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-11-26 08:45:43 +0530

    Time: 133 ms (5.85%), Space: 193.2 MB (5.4%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,18 +1,18 @@
 class Solution {
-    Long dp[][][];
+    Integer dp[][][];
     int n, m;
     int MOD = 1000000007;
     public int numberOfPaths(int[][] grid, int k) {
         n = grid.length;
         m = grid[0].length;
-        dp = new Long[n][m][k];
-        return (int)solve(grid, 0, 0, 0, k);
+        dp = new Integer[n][m][k];
+        return solve(grid, 0, 0, 0, k);
     }
 
-    public long solve(int[][] grid, int row, int col, int mod, int k) {
+    public int solve(int[][] grid, int row, int col, int mod, int k) {
         if (row >= n || col >= m) return 0;
 
-        mod = (mod + grid[row][col] % k) % k;
+        mod = (mod + grid[row][col]) % k;
 
         if (row == n - 1 && col == m - 1)
             return mod == 0 ? 1 : 0;
@@ -20,8 +20,8 @@ class Solution {
         if (dp[row][col][mod] != null)
             return dp[row][col][mod];
 
-        long right = solve(grid, row, col + 1, mod, k) % MOD;
-        long down = solve(grid, row + 1, col, mod, k) % MOD;
+        int right = solve(grid, row, col + 1, mod, k) % MOD;
+        int down = solve(grid, row + 1, col, mod, k) % MOD;
 
         return dp[row][col][mod] = (right + down) % MOD;
     }
