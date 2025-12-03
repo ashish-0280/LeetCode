2f389125ceb6b266d73403786b6ee68cc1771504
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-12-03 09:04:32 +0530

    Time: 860 ms (72.66%), Space: 293 MB (6.25%) - LeetHub

--- 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
+++ 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
@@ -1,27 +1,18 @@
 class Solution {
-
     public int countTrapezoids(int[][] points) {
-
         int n = points.length;
-
         Map<String, List<int[]>> mid = new HashMap<>();
         Map<String, Map<Integer, Integer>> slope = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             int[] p1 = points[i];
-
             for (int j = i + 1; j < n; j++) {
                 int[] p2 = points[j];
-
                 String midKey = (p1[0] + p2[0]) + "," + (p1[1] + p2[1]);
                 mid.computeIfAbsent(midKey, k -> new ArrayList<>()).add(new int[]{i, j});
-
                 String slopeKey = getSlope(p1, p2);
-
                 slope.putIfAbsent(slopeKey, new HashMap<>());
-
                 Map<Integer, Integer> map = slope.get(slopeKey);
-
                 if (!map.containsKey(i)) {
                     map.put(i, i);
                     map.put(j, i);
@@ -31,62 +22,51 @@ class Solution {
             }
         }
 
-        int result = 0;
-
+        int ans = 0;
         for (Map<Integer, Integer> map : slope.values()) {
-
             Map<Integer, Integer> counts = new HashMap<>();
             for (int val : map.values()) {
                 counts.put(val, counts.getOrDefault(val, 0)+1);
             }
-
             int prevPairs = 0;
             for (int cnt : counts.values()) {
                 int currPairs = cnt * (cnt - 1) / 2;
-                result += prevPairs * currPairs;
+                ans += prevPairs * currPairs;
                 prevPairs += currPairs;
             }
         }
 
-        for (List<int[]> segList : mid.values()) {
-
-            if (segList.size() <= 1) continue;
-
+        for (List<int[]> list : mid.values()) {
+            if (list.size() <= 1) continue;
             Map<String, Integer> slopeCount = new HashMap<>();
-
-            for (int[] seg : segList) {
-                int[] p1 = points[seg[0]];
-                int[] p2 = points[seg[1]];
+            for (int[] segment : list) {
+                int[] p1 = points[segment[0]];
+                int[] p2 = points[segment[1]];
                 String key = getSlope(p1, p2);
                 slopeCount.put(key, slopeCount.getOrDefault(key, 0)+1);
             }
-
             int prev = 0;
             for (int cnt : slopeCount.values()) {
-                result -= prev * cnt;
+                ans -= prev * cnt;
                 prev += cnt;
             }
         }
 
-        return result;
+        return ans;
     }
     public int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
-
     private String getSlope(int[] p1, int[] p2) {
         int dx = p2[0] - p1[0];
         int dy = p2[1] - p1[1];
-
         if (dx == 0) return "vertical";
         if (dy == 0) return "horizontal";
-
         int x = Math.abs(dx);
         int y = Math.abs(dy);
         int g = gcd(x, y);
-
-        String sign = (dx * dy > 0 ? "+" : "-");
-        return (x / g) + sign + (y / g);
+        String sign = ((dx * dy > 0) ? "+" : "-");
+        return (x/g) + sign + (y/g);
     }
 }
