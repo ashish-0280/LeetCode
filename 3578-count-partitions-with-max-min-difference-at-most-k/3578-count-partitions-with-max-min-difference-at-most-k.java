4b1d2f45377312db67bf81c6d4590b0d9e2ca906
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-12-06 09:33:26 +0530

    Time: 42 ms (50.62%), Space: 84.7 MB (19.75%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -2,42 +2,35 @@ class Solution {
     int MOD = 1000000007;
     public int countPartitions(int[] nums, int k) {
         int n = nums.length;
-
         long[] dp = new long[n + 1];
         long[] prefix = new long[n + 1];
-
         dp[0] = 1;
         prefix[0] = 1;
-
-        int[] maxQ = new int[n];
-        int[] minQ = new int[n];
-
-        int maxFront = 0, maxBack = 0;  
-        int minFront = 0, minBack = 0;  
-
+        ArrayDeque<Integer> maxQ = new ArrayDeque<>();
+        ArrayDeque<Integer> minQ = new ArrayDeque<>();
         int left = 0;
-
         for (int right = 0; right < n; right++) {
+            while (!maxQ.isEmpty() && nums[maxQ.peekLast()] <= nums[right]) {
+                maxQ.pollLast();
+            }
+            maxQ.addLast(right);
 
-            while (maxBack > maxFront && nums[maxQ[maxBack - 1]] <= nums[right]) maxBack--;
-            maxQ[maxBack++] = right;
+            while (!minQ.isEmpty() && nums[minQ.peekLast()] >= nums[right]) {
+                minQ.pollLast();
+            }
+            minQ.addLast(right);
 
-            while (minBack > minFront && nums[minQ[minBack - 1]] >= nums[right]) minBack--;
-            minQ[minBack++] = right;
-
-            while (nums[maxQ[maxFront]] - nums[minQ[minFront]] > k) {
-                if (maxQ[maxFront] == left) maxFront++;
-                if (minQ[minFront] == left) minFront++;
+            while (nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > k) {
+                if (maxQ.peekFirst() == left) maxQ.pollFirst();
+                if (minQ.peekFirst() == left) minQ.pollFirst();
                 left++;
             }
 
             long ways = prefix[right];
             if (left > 0) ways = (ways - prefix[left - 1] + MOD) % MOD;
-
             dp[right + 1] = ways;
             prefix[right + 1] = (prefix[right] + dp[right + 1]) % MOD;
         }
-
         return (int) dp[n];
     }
 }
