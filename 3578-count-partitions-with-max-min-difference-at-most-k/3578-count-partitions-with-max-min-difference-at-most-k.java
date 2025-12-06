bd91448ceb42fe76661495c4a6f0306978848708
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2025-12-06 13:58:46 +0530

    Time: 41 ms (61.73%), Space: 80.2 MB (40.74%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -1,36 +1,45 @@
 class Solution {
     int MOD = 1000000007;
+
     public int countPartitions(int[] nums, int k) {
         int n = nums.length;
+        
         long[] dp = new long[n + 1];
-        long[] prefix = new long[n + 1];
         dp[0] = 1;
-        prefix[0] = 1;
+
+        long totalPrefix = 1;  // prefix[right]
+        long windowPrefix = 0; // prefix[left-1]
+
         ArrayDeque<Integer> maxQ = new ArrayDeque<>();
         ArrayDeque<Integer> minQ = new ArrayDeque<>();
+
         int left = 0;
+
         for (int right = 0; right < n; right++) {
-            while (!maxQ.isEmpty() && nums[maxQ.peekLast()] <= nums[right]) {
+
+            while (!maxQ.isEmpty() && nums[maxQ.peekLast()] <= nums[right])
                 maxQ.pollLast();
-            }
             maxQ.addLast(right);
 
-            while (!minQ.isEmpty() && nums[minQ.peekLast()] >= nums[right]) {
+            while (!minQ.isEmpty() && nums[minQ.peekLast()] >= nums[right])
                 minQ.pollLast();
-            }
             minQ.addLast(right);
 
             while (nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > k) {
                 if (maxQ.peekFirst() == left) maxQ.pollFirst();
                 if (minQ.peekFirst() == left) minQ.pollFirst();
+
+                // remove dp[left] from windowPrefix
+                windowPrefix = (windowPrefix + dp[left]) % MOD;
                 left++;
             }
 
-            long ways = prefix[right];
-            if (left > 0) ways = (ways - prefix[left - 1] + MOD) % MOD;
+            long ways = (totalPrefix - windowPrefix + MOD) % MOD;
             dp[right + 1] = ways;
-            prefix[right + 1] = (prefix[right] + dp[right + 1]) % MOD;
+
+            totalPrefix = (totalPrefix + dp[right + 1]) % MOD;
         }
+
         return (int) dp[n];
     }
 }
