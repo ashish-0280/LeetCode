2b69038ebd817be3b661ccc84a47110b10f0c92c
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-07 06:32:50 +0530

    Time: 5 ms (99.22%), Space: 60.1 MB (47.47%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -1,12 +1,12 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     long val;
+ *     int val;
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
- *     TreeNode(long val) { this.val = val; }
- *     TreeNode(long val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -15,26 +15,25 @@
  */
 class Solution {
     long max = Long.MIN_VALUE;
-    int mod = 1000000007;
+    int MOD = 1000000007;
     public int maxProduct(TreeNode root) {
-        long p = sum3(root);
-        sum(root, p);
-        return (int)(max % mod);
+        long sum = totalSum(root);
+        dfs(root, sum);
+        return (int)(max % MOD);
     }
+    public long dfs(TreeNode root, long sum){
+        if(root == null) return 0;
 
-    public long sum(TreeNode root, long p){
-        if(root == null) return 0;
-        long sum1 = sum(root.left, p);
-        long sum2 = sum(root.right, p);
-        long res1 = (p - sum1) * sum1;
-        long res2 = (p - sum2) * sum2;
-        max = Math.max(max, Math.max(res1, res2));
-        return sum1 + sum2 + root.val;
+        long left = dfs(root.left, sum);
+        long right = dfs(root.right, sum);
+        long currSum = left + right + root.val;
+        max = Math.max(max, (sum - currSum) * currSum);
+        return currSum;
     }
-    public long sum3(TreeNode root){
+    public long totalSum(TreeNode root){
         if(root == null) return 0;
-        long sum1 = sum3(root.left);
-        long sum2 = sum3(root.right);
-        return sum1 + sum2 + root.val;
+        long left = totalSum(root.left);
+        long right = totalSum(root.right);
+        return left + right + root.val;
     }
 }
