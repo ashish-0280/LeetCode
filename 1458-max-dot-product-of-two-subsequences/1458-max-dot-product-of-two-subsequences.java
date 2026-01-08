aec414d109a33634410f6e274e22e7e20eb1a84c
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-08 06:03:53 +0530

    Time: 11 ms (59.26%), Space: 48.6 MB (9.63%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -3,18 +3,17 @@ class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length;
         dp = new Integer[n][m];
-        int i = n-1, j = m-1;
-        return solve(i, j, nums1, nums2);
+        return solve(0, 0, nums1, nums2);
     }
     public int solve(int i, int j, int nums1[], int nums2[]){
-        if(i < 0 || j < 0) return Integer.MIN_VALUE;
+        if(i >= nums1.length || j >= nums2.length) return Integer.MIN_VALUE;
 
         if(dp[i][j] != null) return dp[i][j];
 
         int product = nums1[i] * nums2[j];
-        int include = product + Math.max(0, solve(i-1, j-1, nums1, nums2));
-        int exclude1 = solve(i-1, j, nums1, nums2);
-        int exclude2 = solve(i, j-1, nums1, nums2);
+        int include = product + Math.max(0, solve(i+1, j+1, nums1, nums2));
+        int exclude1 = solve(i+1, j, nums1, nums2);
+        int exclude2 = solve(i, j+1, nums1, nums2);
 
         return dp[i][j] = Math.max(include, Math.max(exclude1, exclude2));
     }
