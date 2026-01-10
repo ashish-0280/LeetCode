f004dd78c431db6d0a104635755a9123b4423326
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-10 12:30:51 +0530

    Time: 34 ms (30.73%), Space: 18.4 MB (23.97%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int minimumDeleteSum(string s1, string s2) {
-        dp.assign(s1.size(), vector<int>(s2.size(), -1));
-        return solve(s1, s2, 0, 0);
+    int findLCS(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+        if(i>=s1.size()){
+            int sum=0;
+            for(int k=j;k<s2.size();k++){
+                sum+=s2[k];
+            }
+            return sum;
+        }
+        if(j>=s2.size()){
+            int sum=0;
+            for(int k=i;k<s1.size();k++){
+                sum+=s1[k];
+            }
+            return sum;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return dp[i][j]=findLCS(s1,s2,i+1,j+1,dp);
+        }else{
+            return dp[i][j]=min(findLCS(s1,s2,i,j+1,dp)+s2[j],findLCS(s1,s2,i+1,j,dp)+s1[i]);
+        }
     }
-
-    int solve(string &s1, string &s2, int i, int j) {
-        if (i >= s1.size()) {
-            int sum = 0;
-            for (int k = j; k < s2.size(); k++) sum += s2[k];
-            return sum;
-        }
-        if (j >= s2.size()) {
-            int sum = 0;
-            for (int k = i; k < s1.size(); k++) sum += s1[k];
-            return sum;
-        }
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s1[i] == s2[j])
-            return dp[i][j] = solve(s1, s2, i + 1, j + 1);
-
-        int delete1 = s1[i] + solve(s1, s2, i + 1, j);
-        int delete2 = s2[j] + solve(s1, s2, i, j + 1);
-
-        return dp[i][j] = min(delete1, delete2);
+    int minimumDeleteSum(string s1, string s2) {
+        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
+        int ans=findLCS(s1,s2,0,0,dp);
+        return ans;
     }
 };
