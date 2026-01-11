c17091d620af5169fc29ea277be6940e545a9e68
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-11 07:40:11 +0530

    Time: 69 ms (53.09%), Space: 78.3 MB (41.97%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,49 +1,18 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int nsr[] = new int[heights.length];
-        int nsl[] = new int[heights.length];
-        NSR(heights, nsr);
-        NSL(heights, nsl);
-        int maxArea = Integer.MIN_VALUE;
-        for(int i=0; i<heights.length; i++){
-            int w = Math.abs(nsr[i]-nsl[i]-1);
-            int area = heights[i] * w;
-            maxArea = Math.max(maxArea, area);
+        int n = heights.length;
+        int maxArea = 0;
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0; i<=n; i++){
+            int current = (i == n) ? 0 : heights[i];
+            while(!stk.isEmpty() && current < heights[stk.peek()]){
+                int height = heights[stk.pop()];
+                int left = (stk.isEmpty()) ? -1 : stk.peek();
+                int width = i - left - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stk.push(i);
         }
         return maxArea;
-    }
-    public void NSR(int arr[], int nsr[]){
-        Stack<Integer> s = new Stack<>();
-        int i = arr.length-1;
-        while (i >= 0) {
-            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                nsr[i] = arr.length;
-            }
-            else if (arr[s.peek()] < arr[i]) {
-                nsr[i] = s.peek();
-            }
-            s.push(i);
-            i--;
-        }
-    }
-    public void NSL(int arr[], int nsl[]) {
-        Stack<Integer> s = new Stack<>();
-        int i = 0;
-        while (i < arr.length) {
-            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                nsl[i] = -1;
-            }
-            else if (arr[s.peek()] < arr[i]) {
-                nsl[i] = s.peek();
-            }
-            s.push(i);
-            i++;
-        }
     }
 }
