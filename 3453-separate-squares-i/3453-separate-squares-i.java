bf5380483f43a9d63ed8a61123d22c0e54beefd0
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-13 06:21:01 +0530

    Time: 161 ms (26.32%), Space: 147.8 MB (58.95%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public double separateSquares(int[][] squares) {
+        double y = 0; double totalArea = 0;
+        double min = Double.MAX_VALUE; double max = Double.MIN_VALUE;
+        for(int square[]: squares){
+            double len = square[2];
+            totalArea += len * len;
+            min = Math.min(min, square[1]);
+            max = Math.max(max, square[1] + square[2]);
+        }
+        double target = totalArea/2;
+        double low = min; double high = max;
+        for(int i=0; i<80; i++){
+            double mid = (low + high)/2;
+            if(areaBelow(mid, squares) < target){
+                low = mid;
+            } else {
+                y =  mid;
+                high = mid;
+            }
+        }
+        return y;
+    }
+    public double areaBelow(double mid, int squares[][]){
+        double area = 0;
+        for(int square[]: squares){
+            double y = square[1], len = square[2];
+            if(mid >= y + len){
+                area += len * len;
+            } else if((mid > y) && (mid < y+len)){
+                area += len * (mid - y);
+            }
+        }
+        return area;
+    }
+}
