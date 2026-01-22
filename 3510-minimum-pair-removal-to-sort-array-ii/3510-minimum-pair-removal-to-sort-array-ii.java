60f49063cfcfe116bccad5b8f60a8bfebb841fd5
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-22 07:19:39 +0530

    Time: 702 ms (59.79%), Space: 138.1 MB (47.62%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.java
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.java
@@ -1,52 +1,52 @@
 class Solution {
 
     class Pair implements Comparable<Pair> {
-        int l;
+        int idx;
         long val, sum;
         Pair left, right;
 
-        Pair(int l, long val) {
-            this.l = l;
+        Pair(int idx, long val) {
+            this.idx = idx;
             this.val = val;
         }
-        public int compareTo(Pair o) {
-            if (this.right == null || o.right == null) {
+        public int compareTo(Pair curr) {
+            if (this.right == null || curr.right == null) {
                 return this.right == null ? 1 : -1;
             }
-            long d = this.sum - o.sum;
-            return d != 0 ? (d < 0 ? -1 : 1) : (this.l - o.l);
+            long d = this.sum - curr.sum;
+            return d != 0 ? (d < 0 ? -1 : 1) : (this.idx - curr.idx);
         }
     }
 
     public int minimumPairRemoval(int[] nums) {
 
         TreeSet<Pair> heap = new TreeSet<>();
-        int bad = 0;
+        int mismatches = 0;
 
-        Pair cur = null;
+        Pair prev = null;
         for (int i = 0; i < nums.length; i++) {
-            Pair p = new Pair(i, nums[i]);
-            if (cur == null) {
-                cur = p;
+            Pair curr = new Pair(i, nums[i]);
+            if (prev == null) {
+                prev = curr;
             } else {
-                if (p.val < cur.val) bad++;
-                cur.right = p;
-                p.left = cur;
-                cur.sum = cur.val + p.val;
-                heap.add(cur);
-                cur = p;
+                if (curr.val < prev.val) mismatches++;
+                prev.right = curr;
+                curr.left = prev;
+                prev.sum = prev.val + curr.val;
+                heap.add(prev);
+                prev = curr;
             }
         }
-        heap.add(cur);
+        heap.add(prev);
 
         int ans = 0;
 
-        while (bad > 0) {
+        while (mismatches > 0) {
             ans++;
 
             Pair best = heap.pollFirst();
 
-            if (best.right.val < best.val) bad--;
+            if (best.right.val < best.val) mismatches--;
 
             best.sum = best.val + best.right.sum;
             best.val = best.val + best.right.val;
@@ -55,9 +55,9 @@ class Solution {
             best.right = del.right;
 
             if (del.right != null) {
-                if (del.right.val < del.val) bad--;
+                if (del.right.val < del.val) mismatches--;
                 del.right.left = best;
-                if (best.val > best.right.val) bad++;
+                if (best.val > best.right.val) mismatches++;
             }
 
             heap.remove(del);
@@ -66,8 +66,8 @@ class Solution {
             Pair left = best.left;
             if (left != null) {
                 heap.remove(left);
-                if (left.val > left.sum - left.val) bad--;
-                if (left.val > best.val) bad++;
+                if (left.val > left.sum - left.val) mismatches--;
+                if (left.val > best.val) mismatches++;
                 left.sum = left.val + best.val;
                 left.right = best;
                 heap.add(left);
