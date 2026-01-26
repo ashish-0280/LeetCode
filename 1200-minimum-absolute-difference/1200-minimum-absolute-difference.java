741e96d7652a1cabdba153b117b78258c7fa0062
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-26 05:42:47 +0530

    Time: 21 ms (72.27%), Space: 63.8 MB (76.3%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -3,14 +3,10 @@ class Solution {
         Arrays.sort(arr); int min = Integer.MAX_VALUE; int j = 0;
         List<List<Integer>> list = new ArrayList<>();
         for(int i=0; i<arr.length-1; i++){
-            if(arr[i+1] - arr[i] < min){
-                j = 0; min = arr[i+1] - arr[i];
-                list.clear();
-                list.add(new ArrayList<>());
-                list.get(j).add(arr[i]);
-                list.get(j).add(arr[i+1]);
-                j++;
-            } else if(arr[i+1] - arr[i] == min){
+            min = Math.min(min, arr[i+1] - arr[i]);
+        }
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i+1] - arr[i] == min){
                 list.add(new ArrayList<>());
                 list.get(j).add(arr[i]);
                 list.get(j).add(arr[i+1]);
