f332a88f36a6058f370336b9c7e1008d6ff1472e
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-01-27 08:19:19 +0530

    Time: 113 ms (55.47%), Space: 273.5 MB (51.41%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
@@ -2,8 +2,7 @@ class Solution {
     class Pair{
         int x; long c;
         public Pair(int x, long c){
-            this.x = x;
-            this.c = c;
+            this.x = x; this.c = c;
         }
     }
     public int minCost(int n, int[][] edges) {
@@ -13,35 +12,38 @@ class Solution {
         for(int i=0; i<n; i++){
             graph.add(new ArrayList<>());
             rev.add(new ArrayList<>());
+
+        }
+        for(int i=0; i<edges.length; i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int c = edges[i][2];
+            graph.get(a).add(new int[]{b, c});
+            rev.get(b).add(new int[]{a, 2 * c});
         }
 
-        for(int i=0; i<edges.length; i++){
-            int a = edges[i][0]; int b = edges[i][1]; int c = edges[i][2];
-            graph.get(a).add(new int[]{b, c});
-            rev.get(b).add(new int[]{a, 2*c});
-        }
         long d[] = new long[n];
         Arrays.fill(d, Long.MAX_VALUE);
         d[0] = 0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.c, b.c));
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (a, b) -> Long.compare(a.c, b.c)
+        );
         pq.add(new Pair(0, 0L));
         while(!pq.isEmpty()){
             Pair top = pq.poll();
-            int u = top.x;
-            long c = top.c;
+            int u = top.x; long c = top.c;
             if(c > d[u]) continue;
             if(u == n-1) return (int)d[u];
-            for(int neighbour[]: graph.get(u)){
-                int v = neighbour[0];
-                int w = neighbour[1];
+
+            for(int neigh[]: graph.get(u)){
+                int v = neigh[0]; int w = neigh[1];
                 if(d[u] + w < d[v]){
                     d[v] = d[u] + w;
                     pq.add(new Pair(v, d[v]));
                 }
             }
-            for(int neighbour[]: rev.get(u)){
-                int v = neighbour[0];
-                int w = neighbour[1];
+            for(int neigh[]: rev.get(u)){
+                int v = neigh[0]; int w = neigh[1];
                 if(d[u] + w < d[v]){
                     d[v] = d[u] + w;
                     pq.add(new Pair(v, d[v]));
