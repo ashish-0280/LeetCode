0e34a9170c74174707e9bb164fe6643f35f2fabb
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-02-01 13:10:05 +0530

    Time: 492 ms (100%), Space: 111.7 MB (50%) - LeetHub

--- 3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.java
+++ 3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.java
@@ -22,21 +22,22 @@ class Solution {
         int dumy[] = new int[idx];
         int k = 0;
         for(int i=0; i<idx; i++){
-            int target = arr[i];
+            int target = arr[i]; int ans = k;
             int low = 0; int high = k-1;
             while(low <= high){
                 int mid = (low + high)/2;
-                if(dumy[mid] < target){
-                    low = mid + 1;
-                } else {
+                if(dumy[mid] >= target){
+                    ans = mid;
                     high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }   
             }
-            if(low == k){
-                dumy[low] = arr[i];
+            if(ans == k){
+                dumy[ans] = arr[i];
                 k++;
             } else {
-                dumy[low] = arr[i];
+                dumy[ans] = arr[i];
             }
         }
         return k;
