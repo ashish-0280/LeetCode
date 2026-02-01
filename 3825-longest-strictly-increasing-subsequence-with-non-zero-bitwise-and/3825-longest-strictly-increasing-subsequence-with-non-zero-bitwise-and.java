620b925c142a6c632fd00360d237f33c60c8b2b0
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-02-01 12:28:44 +0530

    Time: 483 ms (100%), Space: 111.4 MB (50%) - LeetHub

---
+++ 3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        int arr[] = new int[n]; 
+        for(int i=0; i<30; i++){
+            int idx = 0;
+            for(int num: nums){
+                if(((num >> i) & 1) == 1){
+                    arr[idx] = num;
+                    idx++;
+                }
+            }
+            int len = lis(arr, idx);
+            if(idx > max){
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+    public int lis(int arr[], int idx){
+        int dumy[] = new int[idx];
+        int k = 0;
+        for(int i=0; i<idx; i++){
+            int target = arr[i];
+            int low = 0; int high = k-1;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(dumy[mid] < target){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }   
+            }
+            if(low == k){
+                dumy[low] = arr[i];
+                k++;
+            } else {
+                dumy[low] = arr[i];
+            }
+        }
+        return k;
+    }
+}
