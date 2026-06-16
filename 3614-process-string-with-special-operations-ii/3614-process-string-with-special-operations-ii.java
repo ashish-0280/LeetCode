c29b0d6c0063451289581ea919498f1eceebbfbc
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-06-16 23:53:39 +0530

    Time: 33 ms (24%), Space: 47.7 MB (88%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public char processStr(String s, long k) {
+        int n = s.length();
+        long[] len = new long[n];
+        long l = 0;
+        
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                l++;
+            } else if (c == '*') {
+                if (l > 0) l--;
+            } else if (c == '#') {
+                l *= 2;
+            } else if (c == '%') {
+            }
+            len[i] = l;
+        }
+        
+        if (k < 0 || k >= len[n - 1]) {
+            return '.';
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            long prevLen = (i == 0) ? 0 : len[i - 1];
+            
+            if (c >= 'a' && c <= 'z') {
+                if (k == prevLen) {
+                    return c;
+                }
+            } else if (c == '*') {
+            } else if (c == '#') {
+                if (k >= prevLen) {
+                    k -= prevLen;
+                }
+            } else if (c == '%') {
+                k = prevLen - 1 - k;
+            }
+        }
+        
+        return '.';
+        
+    }
+}
