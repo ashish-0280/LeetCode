51613a9697790a9b635581e0759261ab8e466d8a
Ashish Raj Singh <120677812+ashish-0280@users.noreply.github.com>
2026-06-20 08:18:52 +0530

    Time: 55 ms (73.53%), Space: 126 MB (11.76%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.java
+++ 1840-maximum-building-height/1840-maximum-building-height.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int maxBuilding(int n, int[][] arrtrictions) {
-        int m = arrtrictions.length;
+    public int maxBuilding(int n, int[][] restrictions) {
+        int m = restrictions.length;
         int[][] arr = new int[m + 2][2];
         for (int i = 0; i < m; i++) {
-            arr[i] = arrtrictions[i];
+            arr[i] = restrictions[i];
         }
         arr[m] = new int[]{1, 0};
         arr[m + 1] = new int[]{n, n - 1};
@@ -14,17 +14,14 @@ class Solution {
         for (int i = 1; i < len; i++) {
             arr[i][1] = Math.min(arr[i][1], arr[i - 1][1] + arr[i][0] - arr[i - 1][0]);
         }
-        
         for (int i = len - 2; i >= 0; i--) {
             arr[i][1] = Math.min(arr[i][1], arr[i + 1][1] + arr[i + 1][0] - arr[i][0]);
         }
-        
         int max = 0;
         for (int i = 1; i < len; i++) {
             int peak = arr[i - 1][1] + (arr[i][0] - arr[i - 1][0] + arr[i][1] - arr[i - 1][1]) / 2;
             max = Math.max(max, peak);
         }
-        
         return max;
     }
 }
